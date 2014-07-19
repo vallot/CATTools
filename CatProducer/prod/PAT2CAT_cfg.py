d2bc8fd449508584bd92f8e7a3d483fc69fcdf62
Taejeong <monttj@gmail.com>
2014-07-19 02:03:15 +0200

    adding sequence for weighted isolation

--- CatProducer/prod/PAT2CAT_cfg.py
+++ CatProducer/prod/PAT2CAT_cfg.py
@@ -13,6 +13,39 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('/store/relval/CMSSW_7_0_6_patch1/RelValZMM_13/GEN-SIM-RECO/PLS170_V7AN1-v1/00000/862FBA61-8702-E411-8EE8-003048D25BA6.root')
 )
 
+## for muon isolation study with weighted method
+print "4508 PR needs to be merged"
+print "git cms-merge-topic 4508"
+ 
+process.load("CommonTools.ParticleFlow.deltaBetaWeights_cff")
+
+from PhysicsTools.PatAlgos.tools.helpers import loadWithPostfix
+loadWithPostfix(process,'RecoMuon.MuonIsolation.muonPFIsolation_cff',"Weighted")
+
+process.patMuonsWeighted = process.patMuons.clone()
+process.catMuonsWeighted = process.catMuons.clone()
+process.catMuonsWeighted.src = 'patMuonsWeighted'
+
+process.muPFIsoDepositNeutralWeighted.ExtractorPSet.inputCandView = 'pfWeightedNeutralHadrons'
+process.muPFIsoDepositGammaWeighted.ExtractorPSet.inputCandView = 'pfWeightedPhotons'
+
+process.patMuonsWeighted.isoDeposits = cms.PSet(
+    pfChargedHadrons = cms.InputTag("muPFIsoDepositChargedWeighted" ),
+    pfChargedAll = cms.InputTag("muPFIsoDepositChargedAllWeighted" ),
+    pfPUChargedHadrons = cms.InputTag("muPFIsoDepositPUWeighted" ),
+    pfNeutralHadrons = cms.InputTag("muPFIsoDepositNeutralWeighted" ),
+    pfPhotons = cms.InputTag("muPFIsoDepositGammaWeighted" ),
+    )
+
+process.patMuonsWeighted.isolationValues = cms.PSet(
+    pfChargedHadrons = cms.InputTag("muPFIsoValueCharged04Weighted"),
+    pfChargedAll = cms.InputTag("muPFIsoValueChargedAll04Weighted"),
+    pfPUChargedHadrons = cms.InputTag("muPFIsoValuePU04Weighted" ),
+    pfNeutralHadrons = cms.InputTag("muPFIsoValueNeutral04Weighted" ),
+    pfPhotons = cms.InputTag("muPFIsoValueGamma04Weighted" ),
+    )
+
+##
 ## ------------------------------------------------------
 #  In addition you usually want to change the following
 #  parameters:
