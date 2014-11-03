6ac14da0b0abd2a79a9096f9d99712c39d82fcd0
Jason Lee <jason.lee@cern.ch>
2014-11-03 23:59:04 +0900

    add vertex for electrons

--- CatProducer/python/CATElectronProducer_cfi.py
+++ CatProducer/python/CATElectronProducer_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 catElectrons = cms.EDProducer("CATElectronProducer",
     # input collection
     src = cms.InputTag("patElectrons"),
-)
+    vertexLabel = cms.InputTag('offlinePrimaryVertices')
+    )
 
