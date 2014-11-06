9b1dab0decd2c99e90914e2925476d1447824589
Jason Lee <jason.lee@cern.ch>
2014-11-06 21:58:54 +0900

    turning off MC for run data

--- CatProducer/test/runCatupling.py
+++ CatProducer/test/runCatupling.py
@@ -1,12 +1,12 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
-runOnMC=True
+runOnMC=False
 postfix = "PFlow"
 jetAlgo="AK5"
 
 from CATTools.CatProducer.catPatSetup_cff import *
 from CATTools.CatProducer.catSetup_cff import *
 catPatConfig(process, runOnMC, postfix, jetAlgo)
-catSetup(process)
+catSetup(process, runOnMC)
 
 process.maxEvents.input = 10
 process.source.fileNames = cms.untracked.vstring(
