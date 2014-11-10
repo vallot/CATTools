bd9e205ebc46ec8cf2759c1ee96301cd48bf7c31
Geonmo <ry840901@gmail.com>
2014-11-10 21:07:21 +0900

    Rename configuration files.

--- CatProducer/prod/crab/top_dilepton/mc/runCatupling.py
+++ CatProducer/prod/crab/top_dilepton/mc/runCatupling.py
@@ -9,7 +9,7 @@ from CATTools.CatProducer.catSetup_cff import *
 catPatConfig(process, runOnMC, postfix, jetAlgo)
 catSetup(process, runOnMC, doSecVertex)
 
-process.maxEvents.input = 100
+process.maxEvents.input = -1
 process.source.fileNames = cms.untracked.vstring(
 'file:/cms/home/jlee/scratch/QCD_Pt-15to3000_Tune4C_Flat_8TeV_pythia8/14085CA3-3DE1-E111-BB95-00266CF270A8.root'
 )
