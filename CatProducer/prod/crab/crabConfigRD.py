75491ff439c61110840f8b7fe37525a5a9be6012
Jason Lee <jason.lee@cern.ch>
2014-11-12 21:51:33 +0900

    crab3 test

---
+++ CatProducer/prod/crab/crabConfigRD.py
@@ -0,0 +1,29 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName   = 'cat_test1'
+config.General.saveLogs = True
+
+config.section_("JobType")
+config.JobType.pluginName  = 'Analysis'
+config.JobType.psetName    = 'runCatupling.py'
+config.JobType.pyCfgParams = ['runOnMC=False']
+
+config.section_("Data")
+# This string determines the primary dataset of the newly-produced outputs.
+config.Data.inputDataset = '/QCD_Pt-15to3000_TuneEE3C_Flat_8TeV_herwigpp/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
+config.Data.publication = False
+
+# This string is used to construct the output dataset name
+config.Data.publishDataName = 'CRAB3-test'
+
+config.section_("Site")
+# Where the output files will be transmitted to
+config.Site.storageSite = 'T2_KR_KNU'
+
+
