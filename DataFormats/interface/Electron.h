8df092d55d85ea76e7b39817dd3f5ad0a79dd51c
Taejeong <monttj@gmail.com>
2014-11-18 23:23:54 +0900

    add isolation 0.3 and 0.4 data format for electron

--- DataFormats/interface/Electron.h
+++ DataFormats/interface/Electron.h
@@ -22,19 +22,70 @@ namespace cat {
     Electron(const reco::LeafCandidate & aElectron); 
     virtual ~Electron();
 
-    double chargedHadronIso() const { return chargedHadronIso_;  }
-    double puChargedHadronIso() const { return puChargedHadronIso_; }
-    double neutralHadronIso() const { return neutralHadronIso_; }
-    double photonIso() const { return photonIso_; }
+    double chargedHadronIso(double dR=0.3) const {
+      if( dR == 0.3){ return chargedHadronIso03_;}
+      else if( dR == 0.4) { return chargedHadronIso04_;}
+      else return -1.0;
+    }
+    double puChargedHadronIso(double dR=0.3) const {
+      if( dR == 0.3) { return puChargedHadronIso03_;}
+      else if( dR == 0.4) { return puChargedHadronIso04_;}
+      else return -1.0;
+    }
+    double neutralHadronIso(double dR=0.3) const {
+      if( dR == 0.3) { return neutralHadronIso03_;}
+      else if( dR == 0.4) { return neutralHadronIso04_;}
+      else return -1.0;
+    }
+    double photonIso(double dR=0.3) const {
+      if( dR == 0.3) { return photonIso03_;}
+      else if( dR == 0.4) { return photonIso04_;}
+      else return -1.0;
+    }
+
+    double absIso(double dR=0.3, double dBetaFactor=0.5) const{
+
+      if(dBetaFactor>0 && puChargedHadronIso(dR)<0) return -1;
+
+      double neutralIso = neutralHadronIso(dR) + photonIso(dR);
+      double corNeutralIso = neutralIso - dBetaFactor * puChargedHadronIso(dR);
+
+      double charged = chargedHadronIso(dR);
+
+      return charged + ( corNeutralIso>0 ? corNeutralIso : 0 ) ;
+    }
+
+    double relIso(double dR=0.3, double dBetaFactor=0.5) const{
+      double abs = absIso(dR, dBetaFactor)/this->pt();
+      return abs >=0 ? abs : -1;
+    }
+
 
     float mva() const { return mva_; }
+
+    void setChargedHadronIso03(double i) { chargedHadronIso03_ = i; }
+    void setPUChargedHadronIso03(double i) { puChargedHadronIso03_ = i; }
+    void setNeutralHadronIso03(double i) { neutralHadronIso03_ = i; }
+    void setPhotonIso03(double i) { photonIso03_ = i; }
+
+    void setChargedHadronIso04(double i) { chargedHadronIso04_ = i; }
+    void setPUChargedHadronIso04(double i) { puChargedHadronIso04_ = i; }
+    void setNeutralHadronIso04(double i) { neutralHadronIso04_ = i; }
+
+    void setMva(float m) { mva_ = m; } 
   
   private:
 
-    double chargedHadronIso_;
-    double puChargedHadronIso_;
-    double neutralHadronIso_;
-    double photonIso_;
+    double chargedHadronIso03_;
+    double puChargedHadronIso03_;
+    double neutralHadronIso03_;
+    double photonIso03_;
+
+    double chargedHadronIso04_;
+    double puChargedHadronIso04_;
+    double neutralHadronIso04_;
+    double photonIso04_;
+
 
     float mva_;
 
