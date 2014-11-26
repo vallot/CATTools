44cfcbc81318ea580d93f949c27500d318a81038
hyunyong <antilogy33@gmail.com>
2014-11-26 17:14:27 +0900

    add New JEC correction db

--- CatProducer/test/runCatupling.py
+++ CatProducer/test/runCatupling.py
@@ -19,6 +19,23 @@ from CATTools.CatProducer.catSetup_cff import *
 catPatConfig(process, runOnMC, postfix, jetAlgo)
 catSetup(process, runOnMC, doSecVertex)
 
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+from CondCore.DBCommon.CondDBSetup_cfi import *
+process.jec = cms.ESSource("PoolDBESSource",
+      DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0)
+        ),
+      timetype = cms.string('runnumber'),
+      toGet = cms.VPSet(
+      cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            tag    = cms.string('JetCorrectorParametersCollection_Winter14_V5_DATA_AK5PF'),
+            ),
+      ), 
+      connect = cms.string('sqlite:Winter14_V5_DATA.db')
+)
+
+process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
 process.maxEvents.input = 100
 process.MessageLogger.cerr.threshold = ''
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
