5dad8c43dc3e935dc0e9b3e8db22b3727cea4564
Jason Lee <jason.lee@cern.ch>
2014-12-16 22:20:44 +0900

    bug fix runCat.py

--- CatProducer/prod/runCat.py
+++ CatProducer/prod/runCat.py
@@ -4,47 +4,42 @@ doSecVertex=True # for jpsi candidates
     
 ## setting up arguements
 from FWCore.ParameterSet.VarParsing import VarParsing
-options = VarParsing ('python')
-options.register('runOnMC', True, VarParsing.multiplicity.singleton, VarParsing.varType.bool, "runOnMC: 1  default")
-options.register('useMiniAOD', True, VarParsing.multiplicity.singleton, VarParsing.varType.bool, "useMiniAOD: 1  default")
-options.register('globalTag', '', VarParsing.multiplicity.singleton, VarParsing.varType.string, "globalTag: 1  default")
+options = VarParsing('python')
+options.register('runOnMC', True, VarParsing.multiplicity.singleton, VarParsing.varType.bool, "runOnMC: True default")
+options.register('globalTag', '', VarParsing.multiplicity.singleton, VarParsing.varType.string, "globalTag")
 
 options.parseArguments()
 runOnMC = options.runOnMC
-useMiniAOD = options.useMiniAOD
 globalTag = options.globalTag
 
-print "runOnMC =",runOnMC,"and useMiniAOD =",useMiniAOD
+postfix = "PFlow"
+jetAlgo="AK5"
+from CATTools.CatProducer.catPatSetup_cff import *
+catPatConfig(process, runOnMC, postfix, jetAlgo)
 
-from Configuration.AlCa.GlobalTag import GlobalTag
-if useMiniAOD:
-    if runOnMC:
-        process.GlobalTag = GlobalTag(process.GlobalTag, 'PLS170_V7AN2::All', '')
-    else:
-        process.GlobalTag = GlobalTag(process.GlobalTag, 'GR_70_V2_AN1::All', '')
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+from CondCore.DBCommon.CondDBSetup_cfi import *
+process.jec = cms.ESSource("PoolDBESSource",
+      DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0)
+        ),
+      timetype = cms.string('runnumber'),
+      toGet = cms.VPSet(
+      cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            tag    = cms.string('JetCorrectorParametersCollection_Winter14_V5_DATA_AK5PF'),
+            ),
+      ), 
+      connect = cms.string('sqlite:Winter14_V5_DATA.db')
+)
 
-####################################################################################################
-## from miniAOD/patTuple_mini.py to run miniAOD maker when starting from AOD
-if not useMiniAOD:
-    if not globalTag:
-        print "ERROR!!!! Need correct globalTag to run on AOD"
-    process.GlobalTag = GlobalTag(process.GlobalTag, globalTag, '')
-    process.load('Configuration.StandardSequences.PAT_cff')
-    process.load("RecoVertex.AdaptiveVertexFinder.inclusiveVertexing_cff")
-    from PhysicsTools.PatAlgos.slimming.miniAOD_tools import *
-    if runOnMC:
-        miniAOD_customizeAllMC(process)
-    else :
-        miniAOD_customizeAllData(process)
-####################################################################################################
-## setting up catTools
-print "process.GlobalTag.globaltag =",process.GlobalTag.globaltag
-process.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
+process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+
 
 from CATTools.CatProducer.catSetup_cff import *
 catSetup(process, runOnMC, doSecVertex)
 
-process.maxEvents.input = 100
+process.maxEvents.input = options.maxEvents
 
 process.source.fileNames = options.inputFiles
 
