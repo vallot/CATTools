6447790e18bc24c06ea178171798ae20f6dd6558
Junghwan John Goh <jhgoh@cern.ch>
2014-12-31 01:38:36 +0900

    correct typenames for CompositePtrCandidate

--- PhysicsAnalysis/src/classes.h
+++ PhysicsAnalysis/src/classes.h
@@ -1,15 +1,23 @@
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Candidate/interface/CompositeRefCandidate.h"
 #include "DataFormats/Candidate/interface/CompositePtrCandidate.h"
 #include <vector>
+
+namespace reco {
+  typedef std::vector<CompositePtrCandidate> CompositePtrCandidateCollection;
+  typedef edm::Ref<CompositePtrCandidateCollection> CompositePtrCandidateRef;
+  typedef edm::RefVector<CompositePtrCandidateCollection> CompositePtrCandidateRefVector;
+}
 
 namespace {
 
 struct CATTools_PhysicsAnalysis_DataFormats {
-    reco::CompositePtrCandidate a_;
-    std::vector<reco::CompositePtrCandidate> av;
-    edm::Wrapper<reco::CompositePtrCandidate> aw;
-    edm::Wrapper<std::vector<reco::CompositePtrCandidate> > avw;
-    edm::Ptr<reco::CompositePtrCandidate> aPtr;
+  edm::Wrapper<reco::CompositePtrCandidateCollection> a1;
+  edm::reftobase::Holder<reco::Candidate, reco::CompositePtrCandidateRef> a2;
+  edm::reftobase::RefHolder<reco::CompositePtrCandidateRef> a3;
+  edm::reftobase::VectorHolder<reco::Candidate, reco::CompositePtrCandidateRefVector> a4;
+  edm::reftobase::RefVectorHolder<reco::CompositePtrCandidateRefVector> a5;
+
 };
 
 }
