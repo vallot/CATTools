12b2b57416210799503ca5ac97eecdb291c42dc3
Jason Lee <jason.lee@cern.ch>
2015-02-03 15:55:24 +0100

    reverting back to individual candidate producers

---
+++ CatProducer/python/electronProducer_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+catElectrons = cms.EDProducer("CATElectronProducer",
+    src = cms.InputTag("patElectrons"),
+    shiftedEnDownSrc = cms.InputTag("shiftedSlimmedElectronsEnDown"),
+    shiftedEnUpSrc = cms.InputTag("shiftedSlimmedElectronsEnUp"),
+    mcLabel = cms.InputTag("genParticles"),
+    vertexLabel = cms.InputTag('offlinePrimaryVertices'),
+    beamLineSrc = cms.InputTag("offlineBeamSpot"),
+    rhoLabel = cms.InputTag("fixedGridRhoAll", "rho"),
+    electronIDNames = cms.vstring()
+)
