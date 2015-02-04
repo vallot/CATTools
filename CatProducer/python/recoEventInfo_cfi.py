9c388a58ce42817e9e5c1c2041cb09700f7d020e
Jason Lee <jason.lee@cern.ch>
2015-02-04 14:11:17 +0100

    Update recoEventInfo_cfi.py

--- CatProducer/python/recoEventInfo_cfi.py
+++ CatProducer/python/recoEventInfo_cfi.py
@@ -5,7 +5,13 @@ recoEventInfo = cms.EDProducer("RecoEventInfoProducer",
     triggerResults = cms.InputTag("TriggerResults", "", "HLT"),
     HLT = cms.PSet(
         DoubleMu = cms.vstring(
-            "HLT_Mu17_Mu8_v*", "HLT_Mu17_TkMu8_v*"
+            "HLT_Mu17_Mu8_v*", "HLT_Mu17_TkMu8_v*","HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsonVVL_v*"
+        ),
+        DoubleEl = cms.vstring(
+            "HLT_Ele23_Ele12_CaloId_TrackId_Iso_v*"
+        ),
+        DoubleMuEl = cms.vstring(
+            "HLT_Mu23_TrkIsoVVL_Ele12_Gsf_CaloId_TrackId_Iso_MediumWP_v*","HLT_Mu8_TrkIsoVVL_Ele23_Gsf_CaloId_TrackId_Iso_MediumWP_v*"
         ),
     ),
 )
