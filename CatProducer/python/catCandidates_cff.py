619ceb67830854c6e13d8c96d0fcaefdeef921ea
Jason Lee <jason.lee@cern.ch>
2015-02-09 14:51:39 +0100

    fix config files

--- CatProducer/python/catCandidates_cff.py
+++ CatProducer/python/catCandidates_cff.py
@@ -6,22 +6,4 @@ from CATTools.CatProducer.photonProducer_cfi import *
 from CATTools.CatProducer.jetProducer_cfi import *
 from CATTools.CatProducer.metProducer_cfi import *
 from CATTools.CatProducer.tauProducer_cfi import *
-from CATTools.CatProducer.genJetProducer_cfi import *
-#from CATTools.CatProducer.genTopProducer_cfi import *
-#from CATTools.CatProducer.mcParticleProducer_cfi import *
 from CATTools.CatProducer.secondaryVertexProducer_cfi import *
-
-makeCatCandidates =  cms.Sequence( 
-    catMuons*
-    catElectrons*
-    catJets*
-    catMETs*
-    catGenJets
-    #catMCParticles
-    #catPhotons*
-    #catTaus*
-    # MC information below
-    # dont need for now
-    #catGenTops*
-    #catSecVertexs
-) 
--- CatProducer/python/genTopProducer_cfi.py
+++ CatProducer/python/genTopProducer_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 catGenTops = cms.EDProducer("CATGenTopProducer",
     # input collection
-    genJetLabel = cms.InputTag("ak5GenJets"),
+    genJetLabel = cms.InputTag("ak4GenJets"),
     mcParticleLabel = cms.InputTag("genParticles"),
 )
 
