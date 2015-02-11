737e6c909f15627a59618ca82589ee5a2fa66e08
chanwook <hcwhwang@gmail.com>
2015-02-11 10:59:47 +0100

    1D&2D inclusive

--- CatAnalyzer/plugins/CATGenLevelAnalysis.cc
+++ CatAnalyzer/plugins/CATGenLevelAnalysis.cc
@@ -9,6 +9,7 @@
 #include "DataFormats/Math/interface/deltaPhi.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/Utils/interface/TFileDirectory.h"
 
 #include "TH1F.h"
 #include "TH2F.h"
@@ -33,36 +34,63 @@ private:
   double leptonMinPt_, leptonMaxEta_;
   TH2F* hL1PtParVsPseu_;
   TH2F* hL2PtParVsPseu_;
-  TH2F* hJet1PtParVsPseu_;
-  TH2F* hJet2PtParVsPseu_;
-  /*
-  TH1F* hLepton1Pt_;
-  TH1F* hLepton1Eta_;
-  TH1F* hLepton1Phi_;
-  TH1F* hLepton2Pt_;
-  TH1F* hLepton2Eta_;
-  TH1F* hLepton2Phi_;
-
-  TH1F* hJet1Pt_;
-  TH1F* hJet1Eta_;
-  TH1F* hJet1Phi_;
-
-  TH1F* hJet2Pt_;
-  TH1F* hJet2Eta_;
-  TH1F* hJet2Phi_;
-
-  TH1F* hTop1Mass_;
-  TH1F* hTop2Mass_;
-
-  TH1F* hTTMass_;
-  TH1F* hTTDeltaPhi_;
+  TH2F* hB1PtParVsPseu_;
+  TH2F* hB2PtParVsPseu_;
   
-  TH1F* hLeptonJet1Mass_;
-  TH1F* hLeptonJet2Mass_;
-  TH1F* hDileptonMass_;
+  TH1F* hPartonL1Pt_;
+  TH1F* hPartonL1Eta_;
+  TH1F* hPartonL1Phi_;
+  TH1F* hPartonL2Pt_;
+  TH1F* hPartonL2Eta_;
+  TH1F* hPartonL2Phi_;
 
-  TH1F* hLeptonJetDeltaR_;
-  */
+  TH1F* hPartonB1Pt_;
+  TH1F* hPartonB1Eta_;
+  TH1F* hPartonB1Phi_;
+
+  TH1F* hPartonB2Pt_;
+  TH1F* hPartonB2Eta_;
+  TH1F* hPartonB2Phi_;
+
+  TH1F* hPartonTop1Mass_;
+  TH1F* hPartonTop2Mass_;
+
+  TH1F* hPartonTTMass_;
+  TH1F* hPartonTTDeltaPhi_;
+  
+  TH1F* hPartonLB1Mass_;
+  TH1F* hPartonLB2Mass_;
+  TH1F* hPartonDileptonMass_;
+
+  TH1F* hPartonLBDeltaR_;
+  
+  TH1F* hPseudoL1Pt_;
+  TH1F* hPseudoL1Eta_;
+  TH1F* hPseudoL1Phi_;
+  TH1F* hPseudoL2Pt_;
+  TH1F* hPseudoL2Eta_;
+  TH1F* hPseudoL2Phi_;
+
+  TH1F* hPseudoB1Pt_;
+  TH1F* hPseudoB1Eta_;
+  TH1F* hPseudoB1Phi_;
+
+  TH1F* hPseudoB2Pt_;
+  TH1F* hPseudoB2Eta_;
+  TH1F* hPseudoB2Phi_;
+
+  TH1F* hPseudoTop1Mass_;
+  TH1F* hPseudoTop2Mass_;
+
+  TH1F* hPseudoTTMass_;
+  TH1F* hPseudoTTDeltaPhi_;
+  
+  TH1F* hPseudoLB1Mass_;
+  TH1F* hPseudoLB2Mass_;
+  TH1F* hPseudoDileptonMass_;
+
+  TH1F* hPseudoLBDeltaR_;
+  
 };
 
 //using namespace cat;
@@ -85,37 +113,68 @@ CATGenLevelAnalysis::CATGenLevelAnalysis(const edm::ParameterSet& pset)
  
   hL1PtParVsPseu_ = fs->make<TH2F>("hL1PtParVsPseu", "lepton1 P_{T};parton p_{T} (GeV/c);pseudo p_{T} (GeV/c)", 100, 0, 150, 100, 0, 150);
   hL2PtParVsPseu_ = fs->make<TH2F>("hL2PtParVsPseu", "lepton2 P_{T};parton p_{T} (GeV/c);pseudo p_{T} (GeV/c)", 100, 0, 150, 100, 0, 150);
-  hJet1PtParVsPseu_ = fs->make<TH2F>("hJet1PtParVsPseu", "b jet1 P_{T};parton p_{T} (GeV/c);pseudo p_{T} (GeV/c)", 100, 0, 200, 100, 0, 200);
-  hJet2PtParVsPseu_ = fs->make<TH2F>("hJet2PtParVsPseu", "b jet2 P_{T};parton p_{T} (GeV/c);pseudo p_{T} (GeV/c)", 100, 0, 200, 100, 0, 200);
-  /*
-  hLepton1Pt_   = fs->make<TH1F>("hLepton1Pt", "lepton 1 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
-  hLepton1Eta_  = fs->make<TH1F>("hLepton1Eta", "lepton 1 Eta;#eta;Events", 100, -3, 3);
-  hLepton1Phi_  = fs->make<TH1F>("hLepton1Phi", "lepton 1 Phi;#phi;Events", 100, 0, acos(-1));
+  hB1PtParVsPseu_ = fs->make<TH2F>("hB1PtParVsPseu", "b jet1 P_{T};parton p_{T} (GeV/c);pseudo p_{T} (GeV/c)", 100, 0, 200, 100, 0, 200);
+  hB2PtParVsPseu_ = fs->make<TH2F>("hB2PtParVsPseu", "b jet2 P_{T};parton p_{T} (GeV/c);pseudo p_{T} (GeV/c)", 100, 0, 200, 100, 0, 200);
+  
+  TFileDirectory dirParton = fs->mkdir("parton", "parton");
+  hPartonL1Pt_   = dirParton.make<TH1F>("hPartonL1Pt", "parton lepton 1 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
+  hPartonL1Eta_  = dirParton.make<TH1F>("hPartonL1Eta", "parton lepton 1 Eta;#eta;Events", 100, -3, 3);
+  hPartonL1Phi_  = dirParton.make<TH1F>("hPartonL1Phi", "parton lepton 1 Phi;#phi;Events", 100, 0, acos(-1));
 
-  hLepton2Pt_   = fs->make<TH1F>("hLepton2Pt", "lepton 2 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
-  hLepton2Eta_  = fs->make<TH1F>("hLepton2Eta", "lepton 2 Eta;#eta;Events", 100, -3, 3);
-  hLepton2Phi_  = fs->make<TH1F>("hLepton2Phi", "lepton 2 Phi;#phi;Events", 100, 0, acos(-1));
+  hPartonL2Pt_   = dirParton.make<TH1F>("hPartonL2Pt", "parton lepton 2 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
+  hPartonL2Eta_  = dirParton.make<TH1F>("hPartonL2Eta", "parton lepton 2 Eta;#eta;Events", 100, -3, 3);
+  hPartonL2Phi_  = dirParton.make<TH1F>("hPartonL2Phi", "parton lepton 2 Phi;#phi;Events", 100, 0, acos(-1));
 
-  hJet1Pt_      = fs->make<TH1F>("hJet1Pt", "jet 1 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
-  hJet1Eta_     = fs->make<TH1F>("hJet1Eta", "jet 1 eta;#eta;Events", 100, -3, 3);
-  hJet1Phi_     = fs->make<TH1F>("hJet1Phi", "jet 1 phi;#phi;Events", 100, 0, acos(-1));
+  hPartonB1Pt_      = dirParton.make<TH1F>("hPartonB1Pt", "parton jet 1 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
+  hPartonB1Eta_     = dirParton.make<TH1F>("hPartonB1Eta", "parton jet 1 eta;#eta;Events", 100, -3, 3);
+  hPartonB1Phi_     = dirParton.make<TH1F>("hPartonB1Phi", "parton jet 1 phi;#phi;Events", 100, 0, acos(-1));
 
-  hJet2Pt_      = fs->make<TH1F>("hJet2Pt", "jet 2 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
-  hJet2Eta_     = fs->make<TH1F>("hJet2Eta", "jet 2 eta;#eta;Events", 100, -3, 3);
-  hJet2Phi_     = fs->make<TH1F>("hJet2Phi", "jet 2 phi;#phi;Events", 100, 0, acos(-1));
+  hPartonB2Pt_      = dirParton.make<TH1F>("hPartonB2Pt", "parton jet 2 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
+  hPartonB2Eta_     = dirParton.make<TH1F>("hPartonB2Eta", "parton jet 2 eta;#eta;Events", 100, -3, 3);
+  hPartonB2Phi_     = dirParton.make<TH1F>("hPartonB2Phi", "parton jet 2 phi;#phi;Events", 100, 0, acos(-1));
 
-  hTop1Mass_      = fs->make<TH1F>("hTop1Mass", "top 1 mass;Mass (GeV/c^{2};Events",100,100,300);
-  hTop2Mass_      = fs->make<TH1F>("hTop2Mass", "top 2 mass;Mass (GeV/c^{2};Events",100,100,300);
+  hPartonTop1Mass_      = dirParton.make<TH1F>("hPartonTop1Mass", "parton top 1 mass;Mass (GeV/c^{2};Events",100,100,300);
+  hPartonTop2Mass_      = dirParton.make<TH1F>("hPartonTop2Mass", "parton top 2 mass;Mass (GeV/c^{2};Events",100,100,300);
 
-  hTTMass_      = fs->make<TH1F>("hTTMass", "ttbar invariant mass;Mass (GeV/c^{2};Events",100,0,1000);
-  hTTDeltaPhi_  = fs->make<TH1F>("hTTDeltaPhi", "ttbar #Delta#phi;#Delta#phi;Events",100,0,acos(-1));
+  hPartonTTMass_      = dirParton.make<TH1F>("hPartonTTMass", "parton ttbar invariant mass;Mass (GeV/c^{2};Events",100,0,1000);
+  hPartonTTDeltaPhi_  = dirParton.make<TH1F>("hPartonTTDeltaPhi", "parton ttbar #Delta#phi;#Delta#phi;Events",100,0,acos(-1));
 
-  hLeptonJet1Mass_  = fs->make<TH1F>("hLeptonJet1Mass", "lepton 1 jet 1 invariant mass;Mass (GeV/c^{2});Events",100,0,200);
-  hLeptonJet2Mass_  = fs->make<TH1F>("hLeptonJet2Mass", "lepton 2 jet 2 invariant mass;Mass (GeV/c^{2});Events",100,0,200);
-  hDileptonMass_  = fs->make<TH1F>("hDileptonMass", "dilepton invariant mass;Mass (GeV/c^{2});Events",100,0,200);
+  hPartonLB1Mass_  = dirParton.make<TH1F>("hPartonLB1Mass", "parton lepton 1 parton jet 1 invariant mass;Mass (GeV/c^{2});Events",100,0,200);
+  hPartonLB2Mass_  = dirParton.make<TH1F>("hPartonLB2Mass", "parton lepton 2 parton jet 2 invariant mass;Mass (GeV/c^{2});Events",100,0,200);
+  hPartonDileptonMass_  = dirParton.make<TH1F>("hPartonDileptonMass", "parton dilepton invariant mass;Mass (GeV/c^{2});Events",100,0,200);
 
-  hLeptonJetDeltaR_ = fs->make<TH1F>("hLeptonJetDeltaR", "DeltaR;#DeltaR;Events", 100, 0, 5);
-  */
+  hPartonLBDeltaR_ = dirParton.make<TH1F>("hPartonLBDeltaR", "parton DeltaR;#DeltaR;Events", 100, 0, 5);
+
+  TFileDirectory dirPseudo = fs->mkdir("pseudo", "pseudo");
+  
+  hPseudoL1Pt_   = dirPseudo.make<TH1F>("hPseudoL1Pt", "pseudo lepton 1 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
+  hPseudoL1Eta_  = dirPseudo.make<TH1F>("hPseudoL1Eta", "pseudo lepton 1 Eta;#eta;Events", 100, -3, 3);
+  hPseudoL1Phi_  = dirPseudo.make<TH1F>("hPseudoL1Phi", "pseudo lepton 1 Phi;#phi;Events", 100, 0, acos(-1));
+
+  hPseudoL2Pt_   = dirPseudo.make<TH1F>("hPseudoL2Pt", "pseudo lepton 2 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
+  hPseudoL2Eta_  = dirPseudo.make<TH1F>("hPseudoL2Eta", "pseudo lepton 2 Eta;#eta;Events", 100, -3, 3);
+  hPseudoL2Phi_  = dirPseudo.make<TH1F>("hPseudoL2Phi", "pseudo lepton 2 Phi;#phi;Events", 100, 0, acos(-1));
+
+  hPseudoB1Pt_      = dirPseudo.make<TH1F>("hPseudoB1Pt", "pseudo jet 1 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
+  hPseudoB1Eta_     = dirPseudo.make<TH1F>("hPseudoB1Eta", "pseudo jet 1 eta;#eta;Events", 100, -3, 3);
+  hPseudoB1Phi_     = dirPseudo.make<TH1F>("hPseudoB1Phi", "pseudo jet 1 phi;#phi;Events", 100, 0, acos(-1));
+
+  hPseudoB2Pt_      = dirPseudo.make<TH1F>("hPseudoB2Pt", "pseudo jet 2 p_{T};p_{T} (GeV/c);Events", 100, 0, 100);
+  hPseudoB2Eta_     = dirPseudo.make<TH1F>("hPseudoB2Eta", "pseudo jet 2 eta;#eta;Events", 100, -3, 3);
+  hPseudoB2Phi_     = dirPseudo.make<TH1F>("hPseudoB2Phi", "pseudo jet 2 phi;#phi;Events", 100, 0, acos(-1));
+
+  hPseudoTop1Mass_      = dirPseudo.make<TH1F>("hPseudoTop1Mass", "pseudo top 1 mass;Mass (GeV/c^{2};Events",100,100,300);
+  hPseudoTop2Mass_      = dirPseudo.make<TH1F>("hPseudoTop2Mass", "pseudo top 2 mass;Mass (GeV/c^{2};Events",100,100,300);
+
+  hPseudoTTMass_      = dirPseudo.make<TH1F>("hPseudoTTMass", "pseudo ttbar invariant mass;Mass (GeV/c^{2};Events",100,0,1000);
+  hPseudoTTDeltaPhi_  = dirPseudo.make<TH1F>("hPseudoTTDeltaPhi", "pseudo ttbar #Delta#phi;#Delta#phi;Events",100,0,acos(-1));
+
+  hPseudoLB1Mass_  = dirPseudo.make<TH1F>("hPseudoLB1Mass", "pseudo lepton 1 pseudo jet 1 invariant mass;Mass (GeV/c^{2});Events",100,0,200);
+  hPseudoLB2Mass_  = dirPseudo.make<TH1F>("hPseudoLB2Mass", "pseudo lepton 2 pseudo jet 2 invariant mass;Mass (GeV/c^{2});Events",100,0,200);
+  hPseudoDileptonMass_  = dirPseudo.make<TH1F>("hPseudoDileptonMass", "pseudo dilepton invariant mass;Mass (GeV/c^{2});Events",100,0,200);
+
+  hPseudoLBDeltaR_ = dirPseudo.make<TH1F>("hPseudoLBDeltaR", "pseudo DeltaR;#DeltaR;Events", 100, 0, 5);
+  
   // Do the same for the other histograms
 }
 
@@ -193,44 +252,80 @@ void CATGenLevelAnalysis::analyze(const edm::Event& event, const edm::EventSetup
   
   hL1PtParVsPseu_->Fill(partonL1->pt(),pseudoL1->pt());
   hL2PtParVsPseu_->Fill(partonL2->pt(),pseudoL2->pt());
-  hJet1PtParVsPseu_->Fill(partonB1->pt(),pseudoB1->pt());
-  hJet2PtParVsPseu_->Fill(partonB2->pt(),pseudoB2->pt());
+  hB1PtParVsPseu_->Fill(partonB1->pt(),pseudoB1->pt());
+  hB2PtParVsPseu_->Fill(partonB2->pt(),pseudoB2->pt());
 
-  /*
-  hTop1Mass_->Fill(top1->mass());
-  hTop2Mass_->Fill(top2->mass());
-
-  const double ttMass = (top1->p4() + top2->p4()).mass();
-  const double ttDPhi = abs(deltaPhi(top1->phi(), top2->phi()));
-  const double lbMass1 = (l1->p4() + b1->p4()).mass();
-  const double lbMass2 = (l2->p4() + b2->p4()).mass();
-  const double llMass = (l1->p4() + l2->p4()).mass();
-  hLepton1Pt_->Fill(l1->pt());
-  hLepton1Eta_->Fill(l1->eta());
-  hLepton1Phi_->Fill(l1->phi());
-
-  hLepton2Pt_->Fill(l2->pt());
-  hLepton2Eta_->Fill(l2->eta());
-  hLepton2Phi_->Fill(l2->phi());
-
-  hJet1Pt_->Fill(b1->pt());
-  hJet1Eta_->Fill(b1->eta());
-  hJet1Phi_->Fill(b1->phi());
-
-  hJet2Pt_->Fill(b2->pt());
-  hJet2Eta_->Fill(b2->eta());
-  hJet2Phi_->Fill(b2->phi());
-
-  hTTMass_->Fill(ttMass);
-  hTTDeltaPhi_->Fill(ttDPhi);
   
-  hLeptonJet1Mass_->Fill(lbMass1);
-  hLeptonJet2Mass_->Fill(lbMass2);
-  hDileptonMass_->Fill(llMass);
+  hPartonTop1Mass_->Fill(partonTop1->mass());
+  hPartonTop2Mass_->Fill(partonTop2->mass());
 
-  hLeptonJetDeltaR_->Fill(deltaR(l1->p4(),b1->p4()));    
-  hLeptonJetDeltaR_->Fill(deltaR(l2->p4(),b2->p4()));    
-  */
+  const double partonTTMass = (partonTop1->p4() + partonTop2->p4()).mass();
+  const double partonTTDPhi = abs(deltaPhi(partonTop1->phi(), partonTop2->phi()));
+  const double partonLBMass1 = (partonL1->p4() + partonB1->p4()).mass();
+  const double partonLBMass2 = (partonL2->p4() + partonB2->p4()).mass();
+  const double partonLLMass = (partonL1->p4() + partonL2->p4()).mass();
+
+  hPartonL1Pt_->Fill(partonL1->pt());
+  hPartonL1Eta_->Fill(partonL1->eta());
+  hPartonL1Phi_->Fill(partonL1->phi());
+
+  hPartonL2Pt_->Fill(partonL2->pt());
+  hPartonL2Eta_->Fill(partonL2->eta());
+  hPartonL2Phi_->Fill(partonL2->phi());
+
+  hPartonB1Pt_->Fill(partonB1->pt());
+  hPartonB1Eta_->Fill(partonB1->eta());
+  hPartonB1Phi_->Fill(partonB1->phi());
+
+  hPartonB2Pt_->Fill(partonB2->pt());
+  hPartonB2Eta_->Fill(partonB2->eta());
+  hPartonB2Phi_->Fill(partonB2->phi());
+
+  hPartonTTMass_->Fill(partonTTMass);
+  hPartonTTDeltaPhi_->Fill(partonTTDPhi);
+  
+  hPartonLB1Mass_->Fill(partonLBMass1);
+  hPartonLB2Mass_->Fill(partonLBMass2);
+  hPartonDileptonMass_->Fill(partonLLMass);
+
+  hPartonLBDeltaR_->Fill(deltaR(partonL1->p4(),partonB1->p4()));    
+  hPartonLBDeltaR_->Fill(deltaR(partonL2->p4(),partonB2->p4()));    
+  
+  hPseudoTop1Mass_->Fill(pseudoTop1->mass());
+  hPseudoTop2Mass_->Fill(pseudoTop2->mass());
+
+  const double pseudoTTMass = (pseudoTop1->p4() + pseudoTop2->p4()).mass();
+  const double pseudoTTDPhi = abs(deltaPhi(pseudoTop1->phi(), pseudoTop2->phi()));
+  const double pseudoLBMass1 = (pseudoL1->p4() + pseudoB1->p4()).mass();
+  const double pseudoLBMass2 = (pseudoL2->p4() + pseudoB2->p4()).mass();
+  const double pseudoLLMass = (pseudoL1->p4() + pseudoL2->p4()).mass();
+
+  hPseudoL1Pt_->Fill(pseudoL1->pt());
+  hPseudoL1Eta_->Fill(pseudoL1->eta());
+  hPseudoL1Phi_->Fill(pseudoL1->phi());
+
+  hPseudoL2Pt_->Fill(pseudoL2->pt());
+  hPseudoL2Eta_->Fill(pseudoL2->eta());
+  hPseudoL2Phi_->Fill(pseudoL2->phi());
+
+  hPseudoB1Pt_->Fill(pseudoB1->pt());
+  hPseudoB1Eta_->Fill(pseudoB1->eta());
+  hPseudoB1Phi_->Fill(pseudoB1->phi());
+
+  hPseudoB2Pt_->Fill(pseudoB2->pt());
+  hPseudoB2Eta_->Fill(pseudoB2->eta());
+  hPseudoB2Phi_->Fill(pseudoB2->phi());
+
+  hPseudoTTMass_->Fill(pseudoTTMass);
+  hPseudoTTDeltaPhi_->Fill(pseudoTTDPhi);
+  
+  hPseudoLB1Mass_->Fill(pseudoLBMass1);
+  hPseudoLB2Mass_->Fill(pseudoLBMass2);
+  hPseudoDileptonMass_->Fill(pseudoLLMass);
+
+  hPseudoLBDeltaR_->Fill(deltaR(pseudoL1->p4(),pseudoB1->p4()));    
+  hPseudoLBDeltaR_->Fill(deltaR(pseudoL2->p4(),pseudoB2->p4()));    
+  
 }
 
 DEFINE_FWK_MODULE(CATGenLevelAnalysis);
