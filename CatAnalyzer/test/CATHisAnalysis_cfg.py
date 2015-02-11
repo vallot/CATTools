166d18cae3f60f533fbd3bc543ec8cfa07dc3163
DongHyun <donghyun.kim.linus@gmail.com>
2015-02-11 10:38:51 +0100

    added muon, electron, photon, jet, met and genjets

--- CatAnalyzer/test/CATHisAnalysis_cfg.py
+++ CatAnalyzer/test/CATHisAnalysis_cfg.py
@@ -8,7 +8,6 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("Configuration.StandardSequences.Services_cff")
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
@@ -20,15 +19,19 @@ process.source = cms.Source("PoolSource",
 
 # register TFileService
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('CATMuonAnalysis.root')
+    fileName = cms.string('CATHisAnalysis.root')
 )
 
-process.defaultIso = cms.EDAnalyzer("CATMuonAnalysis",
+process.defaultIso = cms.EDAnalyzer("CATHisAnalysis",
     # input collection
-    muons = cms.InputTag("catMuons"),
     electrons = cms.InputTag("catElectrons"),
-    jets = cms.InputTag("catJet"),
-    met = cms.InputTag("catMET"),
+    genjets = cms.InputTag("catGenJets"),
+    jets = cms.InputTag("catJets"),
+    mets = cms.InputTag("catMETs"),
+    muons = cms.InputTag("catMuons"),
+    photons = cms.InputTag("catPhotons"),
+    #secvertexs = cms.InputTag("catSecVertexs"),
+    taus = cms.InputTag("catTaus"),
 )
 
 
