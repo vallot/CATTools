b510126281128fec60c3c42e4a548ce01f7de8ac
chanwook <hcwhwang@gmail.com>
2015-02-11 13:54:34 +0100

    merged

--- CatAnalyzer/test/run_genLevel_cfg.py
+++ CatAnalyzer/test/run_genLevel_cfg.py
@@ -1,19 +1,20 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.Utilities.FileUtils as FileUtils
 import os
 
 process = cms.Process("Ana")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-#process.load("Configuration.StandardSequences.Services_cff")
-#process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
-process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
+
+#mylist = FileUtils.loadListFromFile ("~/ttbar_miniaod.list")
+#readFiles = cms.untracked.vstring( *mylist)
+#process.source = cms.Source("PoolSource", fileNames = readFiles)
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
 process.source.fileNames = [
-#    'file:/store1/jhgoh/catTuple_790.root',
     'file:/afs/cern.ch/user/c/chanwook/catTuple_290.root',
 ]
 
@@ -21,29 +22,17 @@ process.TFileService = cms.Service("TFileService",
     fileName = cms.string("hist.root"),
 )
 
-process.partontop = cms.EDAnalyzer("CATGenLevelAnalysis",
-    channel = cms.InputTag("partonTop", "channel"),
+process.ana = cms.EDAnalyzer("CATGenLevelAnalysis",
+    channel = cms.InputTag("partonTop","channel"),
     modes = cms.InputTag("partonTop", "modes"),
     partons = cms.InputTag("partonTop"),
+    pseudo = cms.InputTag("pseudoTop"),
     leptonMinPt = cms.double(20.),
     leptonMaxEta = cms.double(2.4),
     jetMinPt = cms.double(20.),
     jetMaxEta = cms.double(2.4),
 )
-
-process.pseudotop = cms.EDAnalyzer("CATGenLevelAnalysis",
-    channel = cms.InputTag("partonTop", "channel"),
-    modes = cms.InputTag("partonTop", "modes"),
-    partons = cms.InputTag("pseudoTop"),
-    leptonMinPt = cms.double(20.),
-    leptonMaxEta = cms.double(2.4),
-    jetMinPt = cms.double(20.),
-    jetMaxEta = cms.double(2.4),
-)
-
 
 process.p = cms.Path(
-    process.partontop+process.pseudotop
+    process.ana
 )
-
-
