c079400275a920d80c92af2dfad04f0f51afbda3
Jason Lee <jason.lee@cern.ch>
2015-02-25 23:51:15 +0900

    Update recoEventInfo_cfi.py

--- CatProducer/python/recoEventInfo_cfi.py
+++ CatProducer/python/recoEventInfo_cfi.py
@@ -14,17 +14,14 @@ recoEventInfo = cms.EDProducer("RecoEventInfoProducer",
         Mu23TrkIsoVVLEle12GsfCaloIdTrackIdIsoMediumWP = cms.vstring("HLT_Mu23_TrkIsoVVL_Ele12_Gsf_CaloId_TrackId_Iso_MediumWP_v*"),
         Mu8TrkIsoVVLEle23GsfCaloIdTrackIdIsoMediumWP = cms.vstring("HLT_Mu8_TrkIsoVVL_Ele23_Gsf_CaloId_TrackId_Iso_MediumWP_v*"),
         ## 8TeV
-        IsoMu20eta2p1 = cms.vstring("HLT_IsoMu20_eta2p1_v*"),
-        Ele27WP80 = cms.vstring("HLT_Ele27_WP80_v*"),
-        ## 5.3E33 [50ns]
         IsoMu24eta2p1 = cms.vstring("HLT_IsoMu24_eta2p1_v*"),
-        Ele27WP75eta2p1 = cms.vstring("HLT_Ele27_WP75_eta2p1_v*"),
-        ## 7E33 [25ns] OOT+in time <mu> = 20
-        IsoMu20eta2p1IterTrk02 = cms.vstring("HLT_IsoMu20_eta2p1_IterTrk02_v*"),
+        Ele27WP80 = cms.vstring("HLT_Ele27_WP80_v*"),
+        ## 5.3E33 [50ns] && 7E33[25ns]
+        IsoMu20eta2p1 = cms.vstring("HLT_IsoMu20_eta2p1_IterTrk02_v*"),
+        Ele27WP75eta2p1 = cms.vstring("HLT_Ele27_eta2p1_WP75_v*"),
         ## 1.4E34 [25ns]
         IsoMu24eta2p1IterTrk02 = cms.vstring("HLT_IsoMu24_eta2p1_IterTrk02_v*"),
-        Ele32WP75eta2p1 = cms.vstring("HLT_Ele32_WP75_eta2p1_v*"),
-        Ele32WP75Gsf = cms.vstring("HLT_Ele32_WP75_Gsf_v*"),
+        Ele32WP75eta2p1 = cms.vstring("HLT_Ele32_eta2p1_WP75_v*"),
     ),
 )
 
