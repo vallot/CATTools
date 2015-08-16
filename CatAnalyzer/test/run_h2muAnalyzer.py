f817d04e4a9fc374a85fe76c2723e37f31b721ab
pseudotop <lsupertopl@gmail.com>
2015-08-17 01:23:28 +0900

    revise a few lines

--- CatAnalyzer/test/run_h2muAnalyzer.py
+++ CatAnalyzer/test/run_h2muAnalyzer.py
@@ -6,30 +6,29 @@ options.parseArguments()
 
 process = cms.Process("h2muAnalyzer")
 savename ="h2mu"
-datadir ="/store/group/CAT/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-3-0_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/150720_065744/0000/"
-
+datadir ="/cms/scratch/CAT/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-3-2_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/150805_203735/0000/"
 if options.dataset == 1:
     savename ="DYJetsToLL_M-50"
-    datadir ="/store/group/CAT/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-3-0_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/150720_065744/0000/"
+    datadir ="/cms/scratch/CAT/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-3-2_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/150805_203735/0000/"
 if options.dataset == 2:
     savename ="TTJets_TuneCUETP8M1_13TeV-madgraphMLM"
-    datadir ="/store/group/CAT/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/v7-3-0_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/150720_065809/0000/"
+    datadir ="/cms/scratch/CAT/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/v7-3-2_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/150805_203745/0000/"
 if options.dataset == 3:
     savename ="ZZ_TuneCUETP8M1_13TeV"
-    datadir ="/store/group/CAT/ZZ_TuneCUETP8M1_13TeV-pythia8/v7-3-0_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/150720_070032/0000/"
+    datadir ="/cms/scratch/CAT/ZZ_TuneCUETP8M1_13TeV-pythia8/v7-3-2_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/150805_203839/0000/"
 if options.dataset == 4:
     savename ="WZ_TuneCUETP8M1_13TeV"
-    datadir ="/store/group/CAT/WZ_TuneCUETP8M1_13TeV-pythia8/v7-3-0_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/150720_070007/0000/"
+    datadir ="/cms/scratch/CAT/WZ_TuneCUETP8M1_13TeV-pythia8/v7-3-2_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/150805_203826/0000/"
 if options.dataset == 5:
     savename ="WW_TuneCUETP8M1_13TeV"
-    datadir ="/store/group/CAT/WW_TuneCUETP8M1_13TeV-pythia8/v7-3-0_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/150720_065935/0000/"
+    datadir ="/cms/scratch/CAT/WW_TuneCUETP8M1_13TeV-pythia8/v7-3-2_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/150805_203816/0000/"
         
 if options.dataset == 6:
     savename ="SingleMuon"
-    datadir ="/store/group/CAT/SingleMuon/v7-3-0_Run2015B-PromptReco-v1/150720_060727/0000/"
+    datadir ="/cms/scratch/CAT/SingleMuon/v7-3-2_Run2015B-PromptReco-v1/150806_164946/0000/"
 if options.dataset == 7:
     savename ="DoubleMuon"
-    datadir ="/store/group/CAT/DoubleMuon/v7-3-0_Run2015B-PromptReco-v1/150720_060849/0000/"
+    datadir ="/cms/scratch/CAT/DoubleMuon/v7-3-2_Run2015B-PromptReco-v1/150806_165018/0000/"
 
 savename+=".root"
 
@@ -42,7 +41,7 @@ for f in os.listdir(datadir):
     process.source.fileNames.append("file:"+datadir+f)
 
 process.h2mu = cms.EDAnalyzer("h2muAnalyzer",
-    vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    vertices = cms.InputTag("catVertex"),
     muons = cms.InputTag("catMuons"),
     electrons = cms.InputTag("catElectrons"),
     jets = cms.InputTag("catJets"),
