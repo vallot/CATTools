5920d2bd10ec629f9da11c0fd1760eb2de82c84c
Junghwan John Goh <jhgoh@cern.ch>
2015-10-01 20:45:18 +0200

    enable AODSIM test

--- CatProducer/test/runtests.sh
+++ CatProducer/test/runtests.sh
@@ -10,10 +10,10 @@ cmsRun ${LOCAL_TEST_DIR}/../prod/PAT2CAT_cfg.py \
   'maxEvents=100' 'runOnMC=1' 'useMiniAOD=1' 'runGenTop=1' 'runOnRelVal=1' \
   || die 'Failure to run PAT2CAT from MiniAODSIM + GenTop' $?
 
-#cmsRun ${LOCAL_TEST_DIR}/../prod/PAT2CAT_cfg.py \
-#  'maxEvents=100' 'runOnMC=1' 'useMiniAOD=0' 'runOnRelVal=1' \
-#  || die 'Failure to run PAT2CAT from AODSIM' $?
+cmsRun ${LOCAL_TEST_DIR}/../prod/PAT2CAT_cfg.py \
+  'maxEvents=100' 'runOnMC=1' 'useMiniAOD=0' 'runOnRelVal=1' \
+  || die 'Failure to run PAT2CAT from AODSIM' $?
 
-#cmsRun ${LOCAL_TEST_DIR}/../prod/PAT2CAT_cfg.py \
-#  'maxEvents=100' 'runOnMC=1' 'useMiniAOD=0' 'runGenTop=1' 'runOnRelVal=1' \
-#  || die 'Failure to run PAT2CAT from AODSIM + GenTop' $?
+cmsRun ${LOCAL_TEST_DIR}/../prod/PAT2CAT_cfg.py \
+  'maxEvents=100' 'runOnMC=1' 'useMiniAOD=0' 'runGenTop=1' 'runOnRelVal=1' \
+  || die 'Failure to run PAT2CAT from AODSIM + GenTop' $?
