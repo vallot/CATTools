bdedb30b84cf80e1c170064b9f87119a4d89b6b4
Junghwan John Goh <jhgoh@cern.ch>
2015-10-20 04:37:43 +0200

    indentation

--- CatAnalyzer/plugins/TtbarSingleLeptonAnalyzer.cc
+++ CatAnalyzer/plugins/TtbarSingleLeptonAnalyzer.cc
@@ -523,33 +523,33 @@ void TtbarSingleLeptonAnalyzer::analyze(const edm::Event& iEvent, const edm::Eve
       if(dr_LepJet > 0.4) cleanJet = true;
 
       if(goodJet && cleanJet){
-  // Basic variables
-  b_Jet_px->push_back(jet.px());
-  b_Jet_py->push_back(jet.py());
-  b_Jet_pz->push_back(jet.pz());
-  b_Jet_E ->push_back(jet.energy());
+        // Basic variables
+        b_Jet_px->push_back(jet.px());
+        b_Jet_py->push_back(jet.py());
+        b_Jet_pz->push_back(jet.pz());
+        b_Jet_E ->push_back(jet.energy());
 
-  // Parton Flavour
-  b_Jet_partonFlavour->push_back(jet.partonFlavour());
-  b_Jet_hadronFlavour->push_back(jet.hadronFlavour());
+        // Parton Flavour
+        b_Jet_partonFlavour->push_back(jet.partonFlavour());
+        b_Jet_hadronFlavour->push_back(jet.hadronFlavour());
 
-  // Smeared and Shifted
-  b_Jet_smearedRes     ->push_back(jet.smearedRes() );
-  b_Jet_smearedResDown ->push_back(jet.smearedResDown());
-  b_Jet_smearedResUp   ->push_back(jet.smearedResUp());
-  b_Jet_shiftedEnUp    ->push_back(jet.shiftedEnUp());
-  b_Jet_shiftedEnDown  ->push_back(jet.shiftedEnDown());
+        // Smeared and Shifted
+        b_Jet_smearedRes     ->push_back(jet.smearedRes() );
+        b_Jet_smearedResDown ->push_back(jet.smearedResDown());
+        b_Jet_smearedResUp   ->push_back(jet.smearedResUp());
+        b_Jet_shiftedEnUp    ->push_back(jet.shiftedEnUp());
+        b_Jet_shiftedEnDown  ->push_back(jet.shiftedEnDown());
 
-  // b-tag discriminant
-  float jet_btagDis_CSV = jet.bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags");
-  b_Jet_CSV ->push_back(jet_btagDis_CSV);
+        // b-tag discriminant
+        float jet_btagDis_CSV = jet.bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags");
+        b_Jet_CSV ->push_back(jet_btagDis_CSV);
 
-  // BTagSFUtil *fBTagSF;   //The BTag SF utility
-  // fBTagSF = new BTagSFUtil("CSVv2", "Medium", 0);
+        // BTagSFUtil *fBTagSF;   //The BTag SF utility
+        // fBTagSF = new BTagSFUtil("CSVv2", "Medium", 0);
 
-  // if (fBTagSF->IsTagged(jet_btagDis_CSV, -999999, jet.pt(), jet.eta()) ){
-  //    std::cout << "Is btag Jet....." << std::endl;
-  // }
+        // if (fBTagSF->IsTagged(jet_btagDis_CSV, -999999, jet.pt(), jet.eta()) ){
+        //    std::cout << "Is btag Jet....." << std::endl;
+        // }
       }
     }
 
