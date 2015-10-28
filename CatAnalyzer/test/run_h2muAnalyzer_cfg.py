1b8d20fb59c8ff682b008b114c1fd7b5a49a404d
Jason Lee <jason.lee@cern.ch>
2015-10-29 07:16:59 +0900

    added submitBatch

--- CatAnalyzer/test/run_h2muAnalyzer_cfg.py
+++ CatAnalyzer/test/run_h2muAnalyzer_cfg.py
@@ -5,28 +5,11 @@ process = cms.Process("h2muAnalyzer")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
-datadir = '/xrootd/store/user/jlee/SingleMuon/v7-4-1_Run2015C-PromptReco-v1/150913_173449/0000/'
-datadir = '/xrootd/store/group/CAT/GluGlu_HToMuMu_M125_13TeV_powheg_pythia8/v7-4-1_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/150917_142416/0000/'
 process.source = cms.Source("PoolSource",
-    #fileNames = cms.untracked.vstring('file:/xrootd/store/user/jlee/SingleMuon/v7-4-1_Run2015C-PromptReco-v1/150913_173449/0000/catTuple_125.root')
-    fileNames = cms.untracked.vstring()
+    #fileNames = cms.untracked.vstring('file:/xrootd/store/group/CAT/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-4-4_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151025_143012/0000/catTuple_85.root')
+    fileNames = cms.untracked.vstring('root://cms-xrdr.sdfarm.kr:1094//xrd/store/group/CAT/DoubleMuon/v7-4-4_Run2015C_25ns-05Oct2015-v1/151023_165157/0000/catTuple_10.root')
+
 )
-
-for f in os.listdir(datadir):
-    process.source.fileNames.append("file:"+datadir+f)
-
-print process.source.fileNames
-runOnMC=True
-### for run data
-lumiFile = 'Cert_246908-255031_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt'
-for i in process.source.fileNames:
-    if 'Run2015' in i:
-        runOnMC=False
-if not runOnMC:
-    from FWCore.PythonUtilities.LumiList import LumiList
-    lumiList = LumiList(os.environ["CMSSW_BASE"]+'/src/CATTools/CatProducer/prod/LumiMask/'+lumiFile)
-    process.source.lumisToProcess = lumiList.getVLuminosityBlockRange()
-    print process.source.lumisToProcess
         
 process.h2mu = cms.EDAnalyzer("h2muAnalyzer",
     vertices = cms.InputTag("catVertex"),
