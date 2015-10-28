313bd983b091b47812bcfd93c5ff277f4ec3f704
Jason Lee <jason.lee@cern.ch>
2015-10-29 07:40:36 +0900

    cleaned up analysis code

--- CatAnalyzer/test/run_TtbarDiLeptonAnalyzer_cfg.py
+++ CatAnalyzer/test/run_TtbarDiLeptonAnalyzer_cfg.py
@@ -1,50 +1,13 @@
 import FWCore.ParameterSet.Config as cms
-import os
-
 process = cms.Process("TtbarDiLeptonAnalyzer")
-process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
-process.options.allowUnscheduled = cms.untracked.bool(True)
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+process.options.allowUnscheduled = cms.untracked.bool(True)
 
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
-
-#datadir = '/xrootd/store/group/CAT/MuonEG/v7-3-6_Run2015B-PromptReco-v1/150922_133849/0000/'
-#datadir = '/xrootd/store/group/CAT/DoubleEG/v7-3-6_Run2015B-PromptReco-v1/150922_133632/0000/'
-#datadir = '/xrootd/store/group/CAT/DoubleMuon/v7-3-6_Run2015B-PromptReco-v1/150922_133736/0000/'
-#
-#for f in os.listdir(datadir):
-#    if ".root" in f:
-#        process.source.fileNames.append("file:"+datadir+f)
-#
-
-#process.source.fileNames = ['file:catTuple.root']
-process.source.fileNames.append('/store/group/CAT/MuonEG/v7-4-2_Run2015C-PromptReco-v1/150923_202331/0000/catTuple_2.root')
-process.source.fileNames.append('/store/group/CAT/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-4-2_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/150923_215647/0001/catTuple_1046.root')
-process.source.fileNames.append('/store/group/CAT/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-4-2_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/150923_215647/0001/catTuple_1047.root')
-process.source.fileNames.append('/store/group/CAT/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-4-2_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/150923_215647/0001/catTuple_1048.root')
-process.source.fileNames.append('/store/group/CAT/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-4-2_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/150923_215647/0001/catTuple_1049.root')
-
-#lumiFile = 'Cert_246908-255031_13TeV_PromptReco_Collisions15_50ns_JSON.txt'
-lumiFile = 'Cert_246908-257599_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
-
-runOnMC = True
-for i in process.source.fileNames:
-    if 'Run2015' in i:
-        runOnMC=False
-if not runOnMC:
-    from FWCore.PythonUtilities.LumiList import LumiList
-    lumiList = LumiList(os.environ["CMSSW_BASE"]+'/src/CATTools/CatProducer/prod/LumiMask/'+lumiFile)    
-    process.source.lumisToProcess = lumiList.getVLuminosityBlockRange()    
-    
-if runOnMC:
-    process.partonTop = cms.EDProducer("PartonTopProducer",
-        genParticles = cms.InputTag("prunedGenParticles"),
-        jetMinPt = cms.double(20),
-        jetMaxEta = cms.double(2.5),
-        jetConeSize = cms.double(0.4),
-    )
+process.source.fileNames.append('root://cms-xrdr.sdfarm.kr:1094//xrd/store/group/CAT/TT_TuneCUETP8M1_13TeV-powheg-pythia8/v7-4-4_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151025_143547/0000/catTuple_96.root')
 
 process.filterRECO = cms.EDFilter("CATTriggerBitCombiner",
     triggerResults = cms.InputTag("TriggerResults::PAT"),
@@ -101,16 +64,14 @@ process.ttll = cms.EDAnalyzer("TtbarDiLeptonAnalyzer",
     muons = cms.InputTag("catMuons"),
     electrons = cms.InputTag("catElectrons"),
     jets = cms.InputTag("catJets"),
-    #mets = cms.InputTag("catMETs"),
-    mets = cms.InputTag("catMETsNoHF"),
+    mets = cms.InputTag("catMETs"),
+    #mets = cms.InputTag("catMETsNoHF"),
     mcLabel = cms.InputTag("prunedGenParticles"),
     
     partonTop_channel = cms.InputTag("partonTop","channel"),
     partonTop_modes = cms.InputTag("partonTop", "modes"),
     partonTop_genParticles = cms.InputTag("partonTop"),
 
-    #isTTbarMC = cms.bool(True),
-    isTTbarMC = cms.bool(False),
     pseudoTop = cms.InputTag("pseudoTop"),
     
     solver = process.ttbarDileptonKinAlgoPSetCMSKin,
--- CatAnalyzer/test/run_h2muAnalyzer_cfg.py
+++ CatAnalyzer/test/run_h2muAnalyzer_cfg.py
@@ -1,17 +1,34 @@
 import FWCore.ParameterSet.Config as cms
-import os
 process = cms.Process("h2muAnalyzer")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+process.options.allowUnscheduled = cms.untracked.bool(True)
 
 process.source = cms.Source("PoolSource",
     #fileNames = cms.untracked.vstring('file:/xrootd/store/group/CAT/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/v7-4-4_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151025_143012/0000/catTuple_85.root')
     fileNames = cms.untracked.vstring('root://cms-xrdr.sdfarm.kr:1094//xrd/store/group/CAT/DoubleMuon/v7-4-4_Run2015C_25ns-05Oct2015-v1/151023_165157/0000/catTuple_10.root')
 
 )
-        
+
+process.filterRECO = cms.EDFilter("CATTriggerBitCombiner",
+    triggerResults = cms.InputTag("TriggerResults::PAT"),
+    secondaryTriggerResults = cms.InputTag("TriggerResults::RECO"),
+    triggerPrescales = cms.InputTag("patTrigger"),
+    combineBy = cms.string("and"),
+    triggersToMatch = cms.vstring(
+        "CSCTightHaloFilter",
+        "EcalDeadCellTriggerPrimitiveFilter",
+        "HBHENoiseFilter",
+        "eeBadScFilter",
+        "goodVertices",
+    ),
+    doFilter = cms.bool(False),
+)
+       
 process.h2mu = cms.EDAnalyzer("h2muAnalyzer",
+    recoFilters = cms.InputTag("filterRECO"),                              
     vertices = cms.InputTag("catVertex"),
     muons = cms.InputTag("catMuons"),
     electrons = cms.InputTag("catElectrons"),
