42146e36fce2bd935e948ff4d0af1ae663e4a05a
Jason Lee <jason.lee@cern.ch>
2015-11-17 23:59:22 +0900

    updated to 745

--- CatAnalyzer/test/TTdilepDraw/topDraw.py
+++ CatAnalyzer/test/TTdilepDraw/topDraw.py
@@ -2,11 +2,11 @@ import ROOT, CATTools.CatAnalyzer.CMS_lumi, json, os,copy
 from CATTools.CatAnalyzer.histoHelper import *
 ROOT.gROOT.SetBatch(True)
 
-datalumi = 1280.23
+datalumi = 1.65
 
 mcfilelist = ['TT_powheg','DYJets','DYJets_10to50']
 rdfilelist = ['MuonEG_Run2015','DoubleEG_Run2015','DoubleMuon_Run2015']
-rootfileDir = "/cms/scratch/jlee/v7-4-4/TtbarDiLeptonAnalyzer_"
+rootfileDir = "/cms/scratch/jlee/v7-4-5/TtbarDiLeptonAnalyzer_"
 datasets = json.load(open("%s/src/CATTools/CatAnalyzer/data/dataset.json" % os.environ['CMSSW_BASE']))
 
 mchistList = []
@@ -14,20 +14,22 @@ mchistList = []
 #binning = [30, 0, 30]
 plotvar = 'll_m'
 binning = [50, 0, 200]
-channel = 2
-step = 4
 x_name = 'mass [GeV]'
 y_name = 'events'
-CMS_lumi.lumi_sqrtS = "%.0f pb^{-1}, #sqrt{s} = 13 TeV 25ns "%(datalumi)
+CMS_lumi.lumi_sqrtS = "%.2f fb^{-1}, #sqrt{s} = 13 TeV"%(datalumi)
 tname = "cattree/nom"
-
+channel = 1
+step = 4
+cut = 'filtered==1&&tri==1'
 weight = 'weight'
+datalumi = datalumi*1000 # due to fb
 if channel == 1: ttother_tcut = "!(parton_channel==2 && ((parton_mode1==1 && parton_mode2==2) || (parton_mode1==2 && parton_mode2==1)))"
 elif channel == 2: ttother_tcut = "!(parton_channel==2 && (parton_mode1==2 && parton_mode2==2))"
 elif channel == 3: ttother_tcut = "!(parton_channel==2 && (parton_mode1==1 && parton_mode2==1))"
-cut = '(step>=%i&&channel==%i&&filtered==1)*%s'%(step,channel,weight)
-ttother_tcut = '(step>=%i && channel == %i && filtered == 1 && %s)*%s'%(step,channel,ttother_tcut,weight)
-print "TCut =",cut
+stepch_tcut =  'step>=%i&&channel==%i'%(step,channel)
+tcut = '(%s&&%s)*%s'%(stepch_tcut,cut,weight)
+ttother_tcut = '(%s&&%s&&%s)*%s'%(stepch_tcut,cut,ttother_tcut,weight)
+print "TCut =",tcut
 
 #DY estimation
 dyratio = [[0 for x in range(6)] for x in range(4)]
@@ -47,27 +49,27 @@ if channel !=1:
     wentries = getWeightedEntries(rfname, tname, "tri",weight)
     scale = scale/wentries
     
-    mc_ee_in = makeTH1(rfname,tname,"mc_ee_in", binning, plotvar, dycut+'(filtered==1 && channel==2 && step2==0)*(%s)'%(weight), scale)
-    mc_mm_in = makeTH1(rfname,tname,"mc_mm_in", binning, plotvar, dycut+'(filtered==1 && channel==3 && step2==0)*(%s)'%(weight), scale)
-    mc_ee_out = makeTH1(rfname,tname,"mc_ee_in", binning, plotvar, dycut+'(filtered==1 && channel==2 && step2==1)*(%s)'%(weight), scale)
-    mc_mm_out = makeTH1(rfname,tname,"mc_mm_in", binning, plotvar, dycut+'(filtered==1 && channel==3 && step2==1)*(%s)'%(weight), scale)
+    mc_ee_in = makeTH1(rfname,tname,"mc_ee_in", binning, plotvar, dycut+'(%s && channel==2 && step2==0)*(%s)'%(cut,weight), scale)
+    mc_mm_in = makeTH1(rfname,tname,"mc_mm_in", binning, plotvar, dycut+'(%s && channel==3 && step2==0)*(%s)'%(cut,weight), scale)
+    mc_ee_out = makeTH1(rfname,tname,"mc_ee_in", binning, plotvar, dycut+'(%s && channel==2 && step2==1)*(%s)'%(cut,weight), scale)
+    mc_mm_out = makeTH1(rfname,tname,"mc_mm_in", binning, plotvar, dycut+'(%s && channel==3 && step2==1)*(%s)'%(cut,weight), scale)
 
     rfname = rootfileDir + 'DYJets_10to50' +".root"
     data = findDataSet('DYJets_10to50', datasets)
     scale = datalumi*data["xsec"]
     wentries = getWeightedEntries(rfname, tname, "tri",weight)
     scale = scale/wentries
-    mc_ee_in.Add(makeTH1(rfname,tname,"mc_ee_in", binning, plotvar, dycut+'(filtered==1 && channel==2 && step2==0)*(%s)'%(weight), scale))
-    mc_mm_in.Add(makeTH1(rfname,tname,"mc_mm_in", binning, plotvar, dycut+'(filtered==1 && channel==3 && step2==0)*(%s)'%(weight), scale))
-    mc_ee_out.Add(makeTH1(rfname,tname,"mc_ee_in", binning, plotvar, dycut+'(filtered==1 && channel==2 && step2==1)*(%s)'%(weight), scale))
-    mc_mm_out.Add(makeTH1(rfname,tname,"mc_mm_in", binning, plotvar, dycut+'(filtered==1 && channel==3 && step2==1)*(%s)'%(weight), scale))
+    mc_ee_in.Add(makeTH1(rfname,tname,"mc_ee_in", binning, plotvar, dycut+'(%s && channel==2 && step2==0)*(%s)'%(cut,weight), scale))
+    mc_mm_in.Add(makeTH1(rfname,tname,"mc_mm_in", binning, plotvar, dycut+'(%s && channel==3 && step2==0)*(%s)'%(cut,weight), scale))
+    mc_ee_out.Add(makeTH1(rfname,tname,"mc_ee_in", binning, plotvar, dycut+'(%s && channel==2 && step2==1)*(%s)'%(cut,weight), scale))
+    mc_mm_out.Add(makeTH1(rfname,tname,"mc_mm_in", binning, plotvar, dycut+'(%s && channel==3 && step2==1)*(%s)'%(cut,weight), scale))
     
     rfname = rootfileDir+rdfilelist[1-1]+".root"
-    rd_em_in = makeTH1(rfname, tname,'rd_ee_in', binning, plotvar, dycut+'(filtered==1 && channel==1 && ((ll_m > 76) && (ll_m < 106)))')
+    rd_em_in = makeTH1(rfname, tname,'rd_em_in', binning, plotvar, dycut+'(%s && channel==1 && ((ll_m > 76) && (ll_m < 106)))'%(cut))
     rfname = rootfileDir + rdfilelist[2-1] +".root"
-    rd_ee_in = makeTH1(rfname, tname,'rd_ee_in', binning, plotvar, dycut+'(filtered==1 && channel==2 && step2 ==0)')
+    rd_ee_in = makeTH1(rfname, tname,'rd_ee_in', binning, plotvar, dycut+'(%s && channel==2 && step2 ==0)'%(cut))
     rfname = rootfileDir + rdfilelist[3-1] +".root"
-    rd_mm_in = makeTH1(rfname, tname,'rd_ee_in', binning, plotvar, dycut+'(filtered==1 && channel==3 && step2 ==0)')
+    rd_mm_in = makeTH1(rfname, tname,'rd_mm_in', binning, plotvar, dycut+'(%s && channel==3 && step2 ==0)'%(cut))
 
     dyest = drellYanEstimation(mc_ee_in.Integral(), mc_ee_out.Integral(), mc_mm_in.Integral(), mc_mm_out.Integral(),
                                rd_ee_in.Integral(), rd_mm_in.Integral(), rd_em_in.Integral())
@@ -88,7 +90,7 @@ for i, mcname in enumerate(mcfilelist):
     wentries = getWeightedEntries(rfname, tname, "tri",weight)
     scale = scale/wentries
     
-    mchist = makeTH1(rfname, tname, title, binning, plotvar, cut, scale)
+    mchist = makeTH1(rfname, tname, title, binning, plotvar, tcut, scale)
     mchist.SetLineColor(colour)
     mchist.SetFillColor(colour)
     mchistList.append(mchist)
@@ -100,7 +102,7 @@ for i, mcname in enumerate(mcfilelist):
         mchist.Add(ttothers, -1)
 
 rfname = rootfileDir + rdfilelist[channel-1] +".root"
-rdhist = makeTH1(rfname, tname, 'data', binning, plotvar, cut)
+rdhist = makeTH1(rfname, tname, 'data', binning, plotvar, tcut)
 rdhist.SetLineColor(1)
 
-drawTH1(plotvar+cut+".png", CMS_lumi, mchistList, rdhist, x_name, y_name, True)
+drawTH1(plotvar+tcut+".png", CMS_lumi, mchistList, rdhist, x_name, y_name, True)
