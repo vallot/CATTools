442e936eeaaf6b745056982c5e2e45f73a102fed
YoungKwon Jo <Young-Kwon.Jo@cern.ch>
2015-11-30 20:33:37 +0100

    added run_TtbarBbbarDiLeptonAnalyzer_cfg.py

---
+++ CatAnalyzer/test/run_TtbarBbbarDiLeptonAnalyzer_cfg.py
@@ -0,0 +1,123 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("TtbarDiLeptonAnalyzer")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+
+process.options.allowUnscheduled = cms.untracked.bool(True)
+
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
+process.source.fileNames.append('root://cms-xrdr.sdfarm.kr:1094//xrd/store/group/CAT/TT_TuneCUETP8M1_13TeV-powheg-pythia8/v7-4-4_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151025_143547/0000/catTuple_96.root')
+#process.source.fileNames.append('root://cms-xrdr.sdfarm.kr:1094//xrd/store/group/CAT/DoubleMuon/v7-4-4_Run2015C_25ns-05Oct2015-v1/151023_165157/0000/catTuple_10.root')
+
+import os
+useGold = True
+isRunData = True
+catmet = 'catMETsNoHF'
+if useGold:
+    catmet = 'catMETs'
+    if isRunData:
+        lumiFile = 'Cert_246908-259891_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+        from FWCore.PythonUtilities.LumiList import LumiList
+        lumiList = LumiList(os.environ["CMSSW_BASE"]+'/src/CATTools/CatProducer/prod/LumiMask/'+lumiFile)
+        process.source.lumisToProcess = lumiList.getVLuminosityBlockRange()
+    
+    process.load("CATTools.CatProducer.pileupWeight_cff")
+    from CATTools.CatProducer.pileupWeight_cff import pileupWeightMap
+    process.pileupWeight.weightingMethod = "RedoWeight"
+    process.pileupWeight.pileupRD = pileupWeightMap["Run2015_25nsV1"]
+    process.pileupWeight.pileupUp = pileupWeightMap["Run2015Up_25nsV1"]
+    process.pileupWeight.pileupDn = pileupWeightMap["Run2015Dn_25nsV1"]
+    
+process.filterRECO = cms.EDFilter("CATTriggerBitCombiner",
+    triggerResults = cms.InputTag("TriggerResults::PAT"),
+    secondaryTriggerResults = cms.InputTag("TriggerResults::RECO"),
+    triggerPrescales = cms.InputTag("patTrigger"),
+    combineBy = cms.string("and"),
+    triggersToMatch = cms.vstring(
+        "CSCTightHaloFilter",
+        #"EcalDeadCellTriggerPrimitiveFilter",
+        #"HBHENoiseFilter",
+        "eeBadScFilter",
+        "goodVertices",
+    ),
+    doFilter = cms.bool(False),
+)
+
+process.filterTrigMUEL = cms.EDFilter("CATTriggerBitCombiner",
+    triggerResults = cms.InputTag("TriggerResults::HLT"),
+    triggerPrescales = cms.InputTag("patTrigger"),
+    combineBy = cms.string("or"),
+    triggersToMatch = cms.vstring(
+        "HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v",
+        "HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v",
+    ),
+    doFilter = cms.bool(False),
+)
+
+process.filterTrigELEL = process.filterTrigMUEL.clone(
+    triggersToMatch = cms.vstring(
+        "HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+ ## "HLT_Ele23_WPLoose_Gsf_v",
+    ),
+)
+
+process.filterTrigMUMU = process.filterTrigMUEL.clone(
+    triggersToMatch = cms.vstring(
+        "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v",
+        "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v",
+ ## "HLT_IsoMu20_v",
+ ## "HLT_IsoTkMu20_v",
+ ## "HLT_IsoMu27_v",
+    ),
+)
+
+#process.load("CATTools.CatAnalyzer.ttbarDileptonKinSolutionAlgos_cff")
+
+process.cattree = cms.EDAnalyzer("TtbarBbbarDiLeptonAnalyzer",
+    recoFilters = cms.InputTag("filterRECO"),
+    nGoodVertex = cms.InputTag("catVertex","nGoodPV"),
+    genweight = cms.InputTag("genWeight","genWeight"),
+    puweight = cms.InputTag("pileupWeight"),
+    trigMUEL = cms.InputTag("filterTrigMUEL"),
+    trigMUMU = cms.InputTag("filterTrigMUMU"),
+    trigELEL = cms.InputTag("filterTrigELEL"),
+
+    vertices = cms.InputTag("catVertex"),
+    muons = cms.InputTag("catMuons"),
+    electrons = cms.InputTag("catElectrons"),
+    jets = cms.InputTag("catJets"),
+    mets = cms.InputTag(catmet),
+    mcLabel = cms.InputTag("prunedGenParticles"),
+    
+    partonTop_channel = cms.InputTag("partonTop","channel"),
+    partonTop_modes = cms.InputTag("partonTop", "modes"),
+    partonTop_genParticles = cms.InputTag("partonTop"),
+
+    pseudoTop = cms.InputTag("pseudoTop"),
+
+    genTtbarId = cms.InputTag("GenTtbarCategories", "genTtbarId"),
+    genTtbarId30 = cms.InputTag("GenTtbarCategories30", "genTtbarId"),
+    genTtbarId40 = cms.InputTag("GenTtbarCategories40", "genTtbarId"),
+
+    GenJets = cms.InputTag("slimmedGenJets"),
+    #solver = process.ttbarDileptonKinAlgoPSetCMSKin,
+    #solver = process.ttbarDileptonKinAlgoPSetDESYSmeared,
+    #solver = process.ttbarDileptonKinAlgoPSetDESYMassLoop,
+)
+#process.cattree.solver.tMassStep = 1
+#if cms.string('DESYSmeared') == process.cattree.solver.algo:
+#    process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+#        cattree = cms.PSet(
+#            initialSeed = cms.untracked.uint32(123456),
+#            engineName = cms.untracked.string('TRandom3')
+#        )
+#    )
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("cattree.root"
+))
+
+process.p = cms.Path(process.cattree)
+process.MessageLogger.cerr.FwkReport.reportEvery = 50000
