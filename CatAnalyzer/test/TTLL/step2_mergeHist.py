38c3389a62255cf7eb8fb6c94935e9d6ed2ac302
Junghwan John Goh <jhgoh@cern.ch>
2015-12-15 11:03:16 +0900

    cleanup after job completion

--- CatAnalyzer/test/TTLL/step2_mergeHist.py
+++ CatAnalyzer/test/TTLL/step2_mergeHist.py
@@ -7,12 +7,38 @@ from os.path import join as pathjoin
 def hadd(d):
     d = d.rstrip('/')
     if not isdir(d): return 
-    rootFiles = [pathjoin(d, x) for x in os.listdir(d) if x.endswith('.root')]
 
-    cmd = 'hadd -f %s.root ' % d
-    cmd += ' '.join(rootFiles)
+    nqueue = 0
+    for l in open(pathjoin(d, 'submit.jds')).readlines():
+        if not l.startswith('queue'): continue
+        if len(l.split()) == 1: nqueue = 1
+        else: nqueue = int(l.split()[-1])
 
-    os.system(cmd)
+    rootFiles = []
+    for x in os.listdir(d):
+        if not x.endswith('.root'): continue
+        x = pathjoin(d, x)
+        if os.stat(x).st_size <= 200: continue
+        rootFiles.append(x)
+
+    if len(rootFiles) == nqueue:
+
+        cmd = 'hadd -f %s.root ' % d
+        cmd += ' '.join(rootFiles)
+
+        print
+        print "*"*40
+        print "*", d, "is complete. Merging and clean up"
+        print "*"*40
+        print
+        os.system(cmd)
+        os.system("rm -rf %s" % d)
+    else:
+        print
+        print "+"*40
+        print "+", d, "is incomplete. wait for the job completion"
+        print "+"*40
+        print
 
 step1Dir = "step1_makeHist"
 outFiles = []
@@ -26,8 +52,13 @@ for sample in os.listdir(step1Dir):
     for category in os.listdir(sample):
         if category == 'central': continue
         category = pathjoin(sample, category)
-        hadd(category)
-        outFiles.append(category+'.root')
+        if not isdir(category): continue
+
+        for direction in os.listdir(category):
+            direction = pathjoin(category, direction)
+            if not isdir(direction): continue
+            hadd(direction)
+            outFiles.append(direction+'.root')
 
 for l in outFiles:
     print l
