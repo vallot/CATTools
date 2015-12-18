8bd50a8f6746d3553606a2565cc954a0ef3540c7
Junghwan John Goh <jhgoh@cern.ch>
2015-12-18 18:50:49 +0900

    print out cross section value

--- CatAnalyzer/test/GenLevel/extractGenDYSF.py
+++ CatAnalyzer/test/GenLevel/extractGenDYSF.py
@@ -2,19 +2,19 @@
 
 from ROOT import *
 
-xHi = 3*2008.4
-xLo20to50 = 3*3205.6-xHi
-doAddSF = False
-#doAddSF = True
+xHi = 3*2008.4 #  Cross section from twiki page, M>50
+xLo20to50 = 3*3205.6-xHi # Cross section from twiki page, M>20
+#doAddSF = False
+doAddSF = True
 
-fHi = TFile("DYJets.root")
-fLo = TFile("DYJets_10to50.root")
-#fHi = TFile("DYJets_MG.root")
-#fLo = TFile("DYJets_MG_5to50.root")
+#fHi = TFile("DYJets.root")
+#fLo = TFile("DYJets_10to50.root")
+fHi = TFile("DYJets_MG.root")
+fLo = TFile("DYJets_MG_5to50.root")
 
 hZMHi = fHi.Get("z/default/hZMass")
 sumwHi = fHi.Get("z/default/hZPhi").Integral()
-hZMHi.Scale(xHi/sumwHi)
+hZMHi.Scale(xHi/sumwHi) ## hZMHi.Integral() becomes total cross section of high mass sample
 hZMHi.SetLineColor(kBlue)
 hZMHi.SetFillColor(kBlue)
 
@@ -23,8 +23,9 @@ sumwLo = fLo.Get("z/default/hZPhi").Integral()
 
 sumwLo20to50 = hZMLo.Integral(hZMLo.FindBin(20.), hZMLo.FindBin(50-0.001))
 sumwLo00to50 = hZMLo.Integral(0, hZMLo.FindBin(50-0.001))
-sfLo = sumwLo20to50/sumwLo00to50
-hZMLo.Scale(xLo20to50/sumwLo/sfLo)
+sfLo = sumwLo00to50/sumwLo20to50 ## SF to extrapolate 20-50 xsec to this sample
+hZMLo.Scale(xLo20to50*sfLo/sumwLo) ## hZMLo.Integral() becomes total cross section of low mass sample
+
 ## Estimate additional factor to patch two region
 ym2 = hZMLo.GetBinContent(hZMLo.FindBin(50-0.001)-1)+hZMHi.GetBinContent(hZMHi.FindBin(50-0.001)-1)
 ym1 = hZMLo.GetBinContent(hZMLo.FindBin(50-0.001))+hZMHi.GetBinContent(hZMHi.FindBin(50-0.001))
@@ -48,4 +49,6 @@ hstack.Draw("hist")
 print "="*40
 print " * Ratio of (Theory)/(Generated) = ", sfLo
 print " * Additional factor for patching two region = ", addSF
+print " * Cross section of DY sample = %.2f" % hZMHi.Integral()
+print " * Cross section of %s sample = %.2f" % (fLo.GetName().replace(".root", ""), hZMLo.Integral())
 print "="*40
