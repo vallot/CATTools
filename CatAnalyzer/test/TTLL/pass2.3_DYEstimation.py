4f1787419a649a3178be427bc8e8125d6f0ce821
Junghwan John Goh <jhgoh@cern.ch>
2015-12-29 21:49:37 +0900

    correction to codes

--- CatAnalyzer/test/TTLL/pass2.3_DYEstimation.py
+++ CatAnalyzer/test/TTLL/pass2.3_DYEstimation.py
@@ -1,53 +1,62 @@
 #!/usr/bin/python
 
 from ROOT import *
-import math
-datalumi = 2.11*1000
+from math import sqrt
+lumi = 2.11*1000
 
 rootfileDir = "pass2/central"
 
-stepList = [1,2,3,4,5]
-
+hName = "ttll/%s/step%d/z_m_noveto"
 fDY = TFile.Open(rootfileDir+"/Z__gamma_rightarrow_ll.root")
 fEM = TFile.Open(rootfileDir+"/MuonEG.root")
 fMM = TFile.Open(rootfileDir+"/DoubleMuon.root")
 fEE = TFile.Open(rootfileDir+"/DoubleEG.root")
 
-rd_ee_out_loose = fEE.Get("/ttll/ee/step1/z_m").Integral()
-rd_ee_in_loose = fEE.Get("/ttll/ee/step1/z_m_noveto").Integral()-rd_ee_out_loose  
+## Calculate efficiency factor kMM and kEE using the step1
+hRD_ee = fEE.Get(hName % ("ee", 1))
+hRD_mm = fMM.Get(hName % ("mm", 1))
+hRD_em = fEM.Get(hName % ("em", 1))
+hDY_ee = fDY.Get(hName % ("ee", 1))
+hDY_mm = fDY.Get(hName % ("mm", 1))
 
-rd_mm_out_loose = fMM.Get("/ttll/mm/step1/z_m").Integral()
-rd_mm_in_loose = fMM.Get("/ttll/mm/step1/z_m_noveto").Integral()-rd_mm_out_loose
+bin1 = hRD_ee.FindBin(91-15+1e-5)
+bin2 = hRD_ee.FindBin(91+15-1e-5)
 
-kEE = math.sqrt(rd_ee_in_loose/rd_mm_in_loose)/2. 
-kMM = math.sqrt(rd_mm_in_loose/rd_ee_in_loose)/2. 
+nRD_ee_in = hRD_ee.Integral(bin1, bin2)
+nRD_mm_in = hRD_mm.Integral(bin1, bin2)
+
+kEE = sqrt(nRD_ee_in/nRD_mm_in)/2.
+kMM = sqrt(nRD_mm_in/nRD_ee_in)/2.
 
 print "kMM = ", kMM, "kEE = ", kEE
 
-for i,step in enumerate(stepList):
-  dy_ee_out = fDY.Get("/ttll/ee/step%s/z_m" % step).Integral()*datalumi
-  dy_ee_in = fDY.Get("/ttll/ee/step%s/z_m_noveto" % step).Integral()*datalumi-dy_ee_out  
+for step in range(1,6):
+    hRD_ee = fEE.Get(hName % ("ee", step))
+    hRD_mm = fMM.Get(hName % ("mm", step))
+    hRD_em = fEM.Get(hName % ("em", step))
+    hDY_ee = fDY.Get(hName % ("ee", step))
+    hDY_mm = fDY.Get(hName % ("mm", step))
 
-  dy_mm_out = fDY.Get("/ttll/mm/step%s/z_m" % step).Integral()*datalumi
-  dy_mm_in = fDY.Get("/ttll/mm/step%s/z_m_noveto" % step).Integral()*datalumi-dy_mm_out 
+    nRD_ee_in = hRD_ee.Integral(bin1, bin2)
+    nRD_mm_in = hRD_mm.Integral(bin1, bin2)
+    nRD_em_in = hRD_em.Integral(bin1, bin2)
+    nDY_ee_in = lumi*hDY_ee.Integral(bin1, bin2)
+    nDY_mm_in = lumi*hDY_mm.Integral(bin1, bin2)
 
-  rd_ee_out = fEE.Get("/ttll/ee/step%s/z_m" % step).Integral()
-  rd_ee_in = fEE.Get("/ttll/ee/step%s/z_m_noveto" % step).Integral()-rd_ee_out  
+    nRD_ee_out = hRD_ee.Integral()-nRD_ee_in
+    nRD_mm_out = hRD_mm.Integral()-nRD_mm_in
+    nRD_em_out = hRD_em.Integral()-nRD_em_in
+    nDY_ee_out = lumi*hDY_ee.Integral()-nDY_ee_in
+    nDY_mm_out = lumi*hDY_mm.Integral()-nDY_mm_in
 
-  rd_mm_out = fMM.Get("/ttll/mm/step%s/z_m" % step).Integral()
-  rd_mm_in = fMM.Get("/ttll/mm/step%s/z_m_noveto" % step).Integral()-rd_mm_out 
+    r_ee = nDY_ee_out/nDY_ee_in
+    r_mm = nDY_mm_out/nDY_mm_in
 
-  rd_em_out = fMM.Get("/ttll/em/step%s/z_m" % step).Integral()
-  rd_em_in = fMM.Get("/ttll/em/step%s/z_m_noveto" % step).Integral()-rd_em_out 
+    nOutEst_ee = r_ee*(nRD_ee_in - nRD_em_in*kEE)
+    nOutEst_mm = r_mm*(nRD_mm_in - nRD_em_in*kMM)
 
-  rDY_ee = dy_ee_out/dy_ee_in 
-  rDY_mm = dy_mm_out/dy_mm_in 
+    scale_ee = nOutEst_ee/nDY_ee_out
+    scale_mm = nOutEst_mm/nDY_mm_out
 
-  nOutEst_ee = rDY_ee*(rd_ee_in - rd_em_in*kEE)
-  nOutEst_mm = rDY_mm*(rd_mm_in - rd_em_in*kMM)
-
-  scale_ee = nOutEst_ee/dy_ee_out
-  scale_mm = nOutEst_mm/dy_mm_out
-
-  print "DY estimation for step", step, "ee =",scale_ee, "mm =",scale_mm
+    print "DY estimation for step", step, "ee =",scale_ee, "mm =",scale_mm
 
