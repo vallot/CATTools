1dc214a03c41e998dda87b4d7d659bd2ac85e9f3
Junghwan John Goh <jhgoh@cern.ch>
2016-01-14 14:48:16 +0100

    taus goes to combined weight

--- CatProducer/plugins/PartonTopProducer.cc
+++ CatProducer/plugins/PartonTopProducer.cc
@@ -276,13 +276,17 @@ void PartonTopProducer::produce(edm::Event& event, const edm::EventSetup& eventS
 
   // Calculate pt-weights
   double ptWeight = 1;
-  if ( tQuarks.size() == 2 ) {
+  if ( tQuarks.size() == 2 and modes->size() == 2 ) {
     const double pt1 = tQuarks.at(0)->pt();
     const double pt2 = tQuarks.at(1)->pt();
 
+    const int mode1 = modes->at(0), mode2 = modes->at(1);
+    const bool isLep1 = (mode1 == CH_MUON or mode2 == CH_ELECTRON);
+    const bool isLep2 = (mode2 == CH_MUON or mode2 == CH_ELECTRON);
+
     double a = 0.156, b = -0.00137;
-    if      ( *channel == CH_FULLLEPTON ) { a = 0.148; b = -0.00129; }
-    else if ( *channel == CH_SEMILEPTON ) { a = 0.159; b = -0.00141; }
+    if ( isLep1 and isLep2 ) { a = 0.148; b = -0.00129; }
+    else if ( isLep1 or isLep2 ) { a = 0.159; b = -0.00141; }
 
     ptWeight = sqrt(exp(a+b*pt1)*exp(a+b*pt2));
   }
