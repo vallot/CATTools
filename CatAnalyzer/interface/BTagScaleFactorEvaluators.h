0ec5dac253f77ba3654b0379ffbed704b790e702
YoungKwonJo <cccpy98@gmail.com>
2016-02-09 02:53:58 +0900

    update bTag Sf for new 20160128

--- CatAnalyzer/interface/BTagScaleFactorEvaluators.h
+++ CatAnalyzer/interface/BTagScaleFactorEvaluators.h
@@ -4,6 +4,10 @@
 #include "CATTools/CatAnalyzer/interface/BTagCalibrationStandalone.h"
 #include "CATTools/DataFormats/interface/Jet.h"
 
+// original code : https://github.com/cms-ttH/MiniAOD/blob/master/MiniAODHelper/interface/CSVHelper.h
+#include "TFile.h"
+#include "TH1D.h"
+
 #include <vector>
 #include <utility>
 #include <algorithm>
@@ -25,9 +29,17 @@ public:
     LFSTAT1_UP, LFSTAT1_DN, LFSTAT2_UP, LFSTAT2_DN,
     CFERR1_UP, CFERR1_DN, CFERR2_UP, CFERR2_DN
   };
-
+  
 private:
   std::map<int, BTagCalibrationReader> readers_;
+
+  void fillCSVHistos(TFile *fileHF, TFile *fileLF,int nHFptBins_);
+
+  // CSV reweighting
+  TH1D *h_csv_wgt_hf[9][6];
+  TH1D *c_csv_wgt_hf[9][6];
+  TH1D *h_csv_wgt_lf[9][4][3];
+  int nHFptBins;
 };
 
 }
--- CatAnalyzer/src/BTagScaleFactorEvaluators.cc
+++ CatAnalyzer/src/BTagScaleFactorEvaluators.cc
@@ -6,6 +6,7 @@ using namespace cat;
 using namespace std;
 CSVWeightEvaluator::CSVWeightEvaluator()
 {
+/*
   const char* method = "iterativefit";
   // setup calibration readers (once)
   const auto csvFile = edm::FileInPath("CATTools/CatAnalyzer/data/scaleFactors/ttH_BTV_CSVv2_13TeV_2015D_20151120.csv").fullPath();
@@ -38,8 +39,92 @@ CSVWeightEvaluator::CSVWeightEvaluator()
 
   readers_[CFERR2_UP] = BTagCalibrationReader(&calib_csvv2, BTagEntry::OP_RESHAPING, method, "up_cferr2");
   readers_[CFERR2_DN] = BTagCalibrationReader(&calib_csvv2, BTagEntry::OP_RESHAPING, method, "down_cferr2");
+*/
+  std::string inputFileHF = "CATTools/CatAnalyzer/data/scaleFactors/csv_rwt_fit_hf_2016_01_28.root";
+  std::string inputFileLF = "CATTools/CatAnalyzer/data/scaleFactors/csv_rwt_fit_lf_2016_01_28.root";
+
+  TFile *f_CSVwgt_HF = new TFile((std::string(getenv("CMSSW_BASE")) + "/src/" + inputFileHF).c_str());
+  TFile *f_CSVwgt_LF = new TFile((std::string(getenv("CMSSW_BASE")) + "/src/" + inputFileLF).c_str());
+
+  fillCSVHistos(f_CSVwgt_HF, f_CSVwgt_LF,6);
 }
 
+double CSVWeightEvaluator::operator()(const cat::Jet& jet, const int unc) const
+{
+  const double pt = std::min(jet.pt(), 999.);
+  const double aeta = std::abs(jet.eta());
+  if ( pt <= 20 or aeta >= 2.4 ) return 1;
+
+  double csv = jet.bDiscriminator(BTAG_CSVv2);
+  if ( csv < 0.0 ) csv = -0.05;
+  else if ( csv > 1.0 ) csv = 1.0;
+
+  const int flav = std::abs(jet.hadronFlavour());
+////////
+int iSysHF = 0;
+    switch (unc) {
+        case CSVWeightEvaluator::JES_UP: iSysHF = 1; break; // JESUp
+        case CSVWeightEvaluator::JES_DN: iSysHF = 2; break; // JESDown
+        case CSVWeightEvaluator::LF_UP: iSysHF = 3;break; // LFUp
+        case CSVWeightEvaluator::LF_DN: iSysHF = 4;  break; // LFDown
+        case CSVWeightEvaluator::HFSTAT1_UP: iSysHF = 5; break; // Stats1Up
+        case CSVWeightEvaluator::HFSTAT1_DN: iSysHF = 6; break; // Stats1Down
+        case CSVWeightEvaluator::HFSTAT2_UP: iSysHF = 7; break; // Stats2Up
+        case CSVWeightEvaluator::HFSTAT2_DN: iSysHF = 8; break; // Stats2Down
+        default: iSysHF = 0;  break; // NoSys
+    }
+
+    int iSysC = 0;
+    switch (unc) {
+        case CSVWeightEvaluator::CFERR1_UP: iSysC = 1; break;
+        case CSVWeightEvaluator::CFERR1_DN: iSysC = 2; break;
+        case CSVWeightEvaluator::CFERR2_UP: iSysC = 3;  break;
+        case CSVWeightEvaluator::CFERR2_DN: iSysC = 4; break;
+        default:  iSysC = 0; break;
+    }
+
+    int iSysLF = 0;
+    switch (unc) {
+        case CSVWeightEvaluator::JES_UP: iSysLF = 1; break; // JESUp
+        case CSVWeightEvaluator::JES_DN: iSysLF = 2; break; // JESDown
+        case CSVWeightEvaluator::HF_UP: iSysLF = 3; break; // HFUp
+        case CSVWeightEvaluator::HF_DN: iSysLF = 4; break; // HFDown
+        case CSVWeightEvaluator::LFSTAT1_UP: iSysLF = 5; break; // Stats1Up
+        case CSVWeightEvaluator::LFSTAT1_DN: iSysLF = 6;break; // Stats1Down
+        case CSVWeightEvaluator::LFSTAT2_UP: iSysLF = 7; break; // Stats2Up
+        case CSVWeightEvaluator::LFSTAT2_DN: iSysLF = 8; break; // Stats2Down
+        default: iSysLF = 0; break; // NoSys
+    }
+
+  int iPt = -1, iEta = -1;
+  if (pt >=19.99 && pt<30) iPt = 0;
+  else if (pt >=30 && pt<40) iPt = 1;
+  else if (pt >=40 && pt<60) iPt = 2;
+  else if (pt >=60 && pt<100) iPt = 3;
+  else if (pt >=100)          iPt = 4;
+
+  if (aeta >= 0 && aeta < 0.8)          iEta = 0;
+  else if (aeta >= 0.8 && aeta < 1.6)   iEta = 1;
+  else if (aeta >= 1.6 && aeta < 2.41)  iEta = 2;
+  else  iEta = 2;
+
+  if (iPt < 0 || iEta < 0) return (float) 1.0;
+
+  if (flav == 5) {
+      if(iPt>=nHFptBins) iPt=nHFptBins-1;
+      int useCSVBin = (csv >= 0.) ? h_csv_wgt_hf[iSysHF][iPt]->FindBin(csv) : 1;
+      return (float) h_csv_wgt_hf[iSysHF][iPt]->GetBinContent(useCSVBin);
+  } else if (flav == 4) {
+      if(iPt>=nHFptBins) iPt=nHFptBins-1;
+      int useCSVBin = (csv >= 0.) ? c_csv_wgt_hf[iSysC][iPt]->FindBin(csv) : 1;
+      return (float) c_csv_wgt_hf[iSysC][iPt]->GetBinContent(useCSVBin);
+  } else {
+      if (iPt >= 3)  iPt = 3; /// [30-40], [40-60] and [60-10000] only 3 Pt bins for lf
+      int useCSVBin = (csv >= 0.) ? h_csv_wgt_lf[iSysLF][iPt][iEta]->FindBin(csv) : 1;
+      return  (float) h_csv_wgt_lf[iSysLF][iPt][iEta]->GetBinContent(useCSVBin);
+  }
+}
+/*
 double CSVWeightEvaluator::operator()(const cat::Jet& jet, const int unc) const
 {
   const double pt = std::min(jet.pt(), 999.);
@@ -75,5 +160,100 @@ double CSVWeightEvaluator::operator()(const cat::Jet& jet, const int unc) const
   if ( reader == readers_.end() ) return 1;
 
   return reader->second.eval(jf, aeta, pt, csv);
+}
+*/
+
+// fill the histograms (done once)
+void CSVWeightEvaluator::fillCSVHistos(TFile *fileHF, TFile *fileLF,int nHFptBins_)
+{
+    nHFptBins=nHFptBins_;
+    for (int iSys = 0; iSys < 9; iSys++) {
+        for (int iPt = 0; iPt < 5; iPt++)
+            h_csv_wgt_hf[iSys][iPt] = NULL;
+        for (int iPt = 0; iPt < 3; iPt++) {
+            for (int iEta = 0; iEta < 3; iEta++)
+                h_csv_wgt_lf[iSys][iPt][iEta] = NULL;
+        }
+    }
+    for (int iSys = 0; iSys < 5; iSys++) {
+        for (int iPt = 0; iPt < 5; iPt++)
+            c_csv_wgt_hf[iSys][iPt] = NULL;
+    }
+
+    // CSV reweighting /// only care about the nominal ones
+    for (int iSys = 0; iSys < 9; iSys++) {
+        TString syst_csv_suffix_hf = "final";
+        TString syst_csv_suffix_c = "final";
+        TString syst_csv_suffix_lf = "final";
+
+        switch (iSys) {
+            case 0:
+                // this is the nominal case
+                break;
+            case 1:
+                // JESUp
+                syst_csv_suffix_hf = "final_JESUp";
+                syst_csv_suffix_lf = "final_JESUp";
+                syst_csv_suffix_c = "final_cErr1Up";
+                break;
+            case 2:
+                // JESDown
+                syst_csv_suffix_hf = "final_JESDown";
+                syst_csv_suffix_lf = "final_JESDown";
+                syst_csv_suffix_c = "final_cErr1Down";
+                break;
+            case 3:
+                // purity up
+                syst_csv_suffix_hf = "final_LFUp";
+                syst_csv_suffix_lf = "final_HFUp";
+                syst_csv_suffix_c = "final_cErr2Up";
+                break;
+            case 4:
+                // purity down
+                syst_csv_suffix_hf = "final_LFDown";
+                syst_csv_suffix_lf = "final_HFDown";
+                syst_csv_suffix_c = "final_cErr2Down";
+                break;
+            case 5:
+                // stats1 up
+                syst_csv_suffix_hf = "final_Stats1Up";
+                syst_csv_suffix_lf = "final_Stats1Up";
+                break;
+            case 6:
+                // stats1 down
+                syst_csv_suffix_hf = "final_Stats1Down";
+                syst_csv_suffix_lf = "final_Stats1Down";
+                break;
+            case 7:
+                // stats2 up
+                syst_csv_suffix_hf = "final_Stats2Up";
+                syst_csv_suffix_lf = "final_Stats2Up";
+                break;
+            case 8:
+                // stats2 down
+                syst_csv_suffix_hf = "final_Stats2Down";
+                syst_csv_suffix_lf = "final_Stats2Down";
+                break;
+        }
+
+        for (int iPt = 0; iPt < nHFptBins; iPt++)
+            h_csv_wgt_hf[iSys][iPt] =
+                (TH1D *)fileHF->Get(Form("csv_ratio_Pt%i_Eta0_%s", iPt, syst_csv_suffix_hf.Data()));
+
+        if (iSys < 5) {
+            for (int iPt = 0; iPt < nHFptBins; iPt++)
+                c_csv_wgt_hf[iSys][iPt] =
+                    (TH1D *)fileHF->Get(Form("c_csv_ratio_Pt%i_Eta0_%s", iPt, syst_csv_suffix_c.Data()));
+        }
+
+        for (int iPt = 0; iPt < 4; iPt++) {
+            for (int iEta = 0; iEta < 3; iEta++)
+                h_csv_wgt_lf[iSys][iPt][iEta] =
+                    (TH1D *)fileLF->Get(Form("csv_ratio_Pt%i_Eta%i_%s", iPt, iEta, syst_csv_suffix_lf.Data()));
+        }
+
+    }
+
+    return;
 }
 
