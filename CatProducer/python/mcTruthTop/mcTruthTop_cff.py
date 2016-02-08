40c4e5d5de126e6b5569677356914b222896842f
Jason Lee <jason.lee@cern.ch>
2016-02-08 19:51:04 +0100

    Update mcTruthTop_cff.py

--- CatProducer/python/mcTruthTop/mcTruthTop_cff.py
+++ CatProducer/python/mcTruthTop/mcTruthTop_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from CATTools.CatProducers.mcTruthTop.partonTop_cfi import *
+from CATTools.CatProducer.mcTruthTop.partonTop_cfi import *
 from TopQuarkAnalysis.TopEventProducers.producers.pseudoTop_cfi import *
 
 from TopQuarkAnalysis.TopTools.GenTtbarCategorizer_cfi import *
