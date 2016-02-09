aaa6bf2270e223f4a260cc5cb41c80e712bbd3b3
Jason Lee <jason.lee@cern.ch>
2016-02-09 16:12:20 +0100

    for 7-6-2 update jec to summer15 v7 update json to rereco

--- CatProducer/python/catDefinitions_cfi.py
+++ CatProducer/python/catDefinitions_cfi.py
@@ -3,10 +3,10 @@ import FWCore.ParameterSet.Config as cms
 bunchCrossing  = 25
 globalTag_mc   = '76X_mcRun2_asymptotic_v12'
 globalTag_rd   = '76X_dataRun2_v15'
-lumiJSON       = 'Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_v2'
-lumiJSONSilver = 'Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_Silver_v2'
+lumiJSON       = 'Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON'
+lumiJSONSilver = 'Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_Silver'
 pileupMCmap    = '2015_25ns_FallMC'
 
-JetEnergyCorrection = 'Summer15_25nsV6'
+JetEnergyCorrection = 'Summer15_25nsV7'
 JECUncertaintyFile  = 'CATTools/CatProducer/data/JEC/%s_DATA_UncertaintySources_AK4PFchs.txt'%JetEnergyCorrection
 
