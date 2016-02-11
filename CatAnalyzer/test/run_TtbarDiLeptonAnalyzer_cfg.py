6e6be36964f3bab5cc534eca2b4b252458622166
Jason Lee <jason.lee@cern.ch>
2016-02-11 19:46:50 +0900

    ttll run -1

--- CatAnalyzer/test/run_TtbarDiLeptonAnalyzer_cfg.py
+++ CatAnalyzer/test/run_TtbarDiLeptonAnalyzer_cfg.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("TtbarDiLeptonAnalyzer")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 
 process.options.allowUnscheduled = cms.untracked.bool(True)
