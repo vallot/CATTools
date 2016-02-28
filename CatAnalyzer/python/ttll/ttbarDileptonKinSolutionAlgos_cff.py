ddae289fd5f095742852630ffd04588c6c612d2d
Jason Lee <jason.lee@cern.ch>
2016-02-29 02:45:13 +0900

    ttll update nuPars

--- CatAnalyzer/python/ttll/ttbarDileptonKinSolutionAlgos_cff.py
+++ CatAnalyzer/python/ttll/ttbarDileptonKinSolutionAlgos_cff.py
@@ -5,7 +5,8 @@ ttbarDileptonKinAlgoPSetCMSKin = cms.PSet(
     tMassBegin = cms.double(100),
     tMassEnd   = cms.double(300),
     tMassStep  = cms.double(0.5),
-    nuPars = cms.vdouble(27.23,53.88,19.92,53.89,19.9), # 13TeV 50ns powheg, produced by Youn
+    nuPars = cms.vdouble(18.41,61.61,23.68,61.64,23.65), # 13TeV 25ns powheg, produced by Youn
+    #nuPars = cms.vdouble(27.23,53.88,19.92,53.89,19.9), # 13TeV 50ns powheg, produced by Youn
     #nuPars = cms.vdouble(30.641,57.941,22.344,57.533,22.232), # 7?8?TeV from DESY
     #nuPars = cms.vdouble(30.7137,56.2880,23.0744,59.1015,24.9145), # 7TeV pythia from CMSSW default
 )
