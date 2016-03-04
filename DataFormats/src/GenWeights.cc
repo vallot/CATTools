cad9ffbce437d9dbfbb07d789f23013390ef22d6
Junghwan John Goh <jhgoh@cern.ch>
2016-03-04 13:57:57 +0100

    first migration of TTLL workflow

--- DataFormats/src/GenWeights.cc
+++ DataFormats/src/GenWeights.cc
@@ -1,4 +1,5 @@
 #include "CATTools/DataFormats/interface/GenWeights.h"
+#include <algorithm>
 
 using namespace std;
 using namespace cat;
@@ -10,6 +11,17 @@ void GenWeightInfo::addWeightGroup(const string typeName, const string combineBy
   weightParams_.push_back(weightParams);
 }
 
+GenWeightInfo::KnownTypes GenWeightInfo::toKnownType(string typeName)
+{
+  std::transform(typeName.begin(), typeName.end(), typeName.begin(), ::tolower);
+
+  if ( typeName == "nominal" ) return Nominal;
+  else if ( typeName == "pdf" ) return PDF;
+  else if ( typeName == "scaleup" ) return ScaleUp;
+  else if ( typeName == "scaledown" ) return ScaleDown;
+
+  return NONE;
+}
 
 /// default constructor
 GenWeights::GenWeights()
@@ -27,5 +39,4 @@ void GenWeights::setInfo(const int id1, const int id2, const float x1, const flo
   x2_ = x2;
   qScale_ = qScale;
 }
-
 
