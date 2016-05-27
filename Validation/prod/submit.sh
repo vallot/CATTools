504a3317c714fbe89ab24cdf8f05cc5717cbdd1f
Junghwan John Goh <jhgoh@cern.ch>
2016-05-28 00:09:23 +0900

    run with splitted jobs

--- Validation/prod/submit.sh
+++ Validation/prod/submit.sh
@@ -4,5 +4,5 @@ for DS in $CMSSW_BASE/src/CATTools/CatAnalyzer/data/dataset/dataset_*.txt; do
   NAME=`basename $DS`
   NAME=${NAME/.txt/}
   NAME=${NAME/dataset_/}
-  create-batch --jobName $NAME --nJobs 1 --fileList $DS --cfg run_validation_cfg.py -n
+  create-batch --jobName $NAME --maxFiles 10 --fileList $DS --cfg run_validation_cfg.py -n
 done
