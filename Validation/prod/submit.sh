7ca615f6caad0fcac9deccb64edc8f2d9acc0b7b
Junghwan John Goh <jhgoh@cern.ch>
2016-06-02 05:24:28 +0900

    remove obsolete

--- Validation/prod/submit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for DS in $CMSSW_BASE/src/CATTools/CatAnalyzer/data/dataset/dataset_*.txt; do
-  NAME=`basename $DS`
-  NAME=${NAME/.txt/}
-  NAME=${NAME/dataset_/}
-  create-batch --jobName $NAME --maxFiles 10 --fileList $DS --cfg run_validation_cfg.py -n
-done
