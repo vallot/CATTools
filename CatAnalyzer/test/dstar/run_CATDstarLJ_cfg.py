70e6bf25407f3734393e176a39bc6bd09dbf34cd
Geonmo <ry840901@gmail.com>
2016-06-20 20:42:01 +0900

    Add input file.

--- CatAnalyzer/test/dstar/run_CATDstarLJ_cfg.py
+++ CatAnalyzer/test/dstar/run_CATDstarLJ_cfg.py
@@ -9,9 +9,11 @@ process.options.allowUnscheduled = cms.untracked.bool(True)
 
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
 #process.source.fileNames = ['/store/user/jhgoh/CATTools/sync/v7-6-3/MuonEG_Run2015D-16Dec2015-v1.root',]
-process.source.fileNames = ['file:/xrootd/store/user/jhgoh/CATTools/sync/v7-6-3/TT_TuneCUETP8M1_13TeV-powheg-pythia8.root',]
+#process.source.fileNames = ['file:/xrootd/store/user/jhgoh/CATTools/sync/v7-6-3/TT_TuneCUETP8M1_13TeV-powheg-pythia8.root',]
 #process.source.fileNames = ['file:../../../catdata_20160315/catTuple.root']
-process.source.fileNames = ['file:catTuple.root']
+#process.source.fileNames = ['file:catTuple.root']
+process.source.fileNames = ['/store/user/geonmo/13TeV_miniaod/Sync/76x/Sync_76X_MC_LJ.root']
+
 
 useSilver = False
 catmet = 'catMETs'
