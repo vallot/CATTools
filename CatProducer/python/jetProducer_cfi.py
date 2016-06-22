743a95f545eed90e4adbda82f0b40a3d899a780a
Junghwan John Goh <jhgoh@cern.ch>
2016-06-22 20:34:44 +0200

    JER for puppi

--- CatProducer/python/jetProducer_cfi.py
+++ CatProducer/python/jetProducer_cfi.py
@@ -14,5 +14,7 @@ catJetsPuppi = catJets.clone(
     src = cms.InputTag('slimmedJetsPuppi'),
     btagNames = cms.vstring('pfCombinedInclusiveSecondaryVertexV2BJetTagsAK4PFPuppi','pfCombinedMVAV2BJetTagsAK4PFPuppi'),
     payloadName = cms.string('AK4PFPuppi'),
+    jetResFile = cms.string("CATTools/CatProducer/data/JER/Fall15_25nsV2_MC_PtResolution_AK4PFPuppi.txt"),
+    jetResSFFile = cms.string("CATTools/CatProducer/data/JER/Fall15_25nsV2_MC_SF_AK4PFPuppi.txt"),
 )
 #There is a CMS rule that we are supposed to use one module per cfi file.  
