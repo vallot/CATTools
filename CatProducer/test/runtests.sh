97688b71033c2fee45a7989f195c0c0430358cd8
Junghwan John Goh <jhgoh@cern.ch>
2016-06-22 14:31:55 +0200

    update input samples

--- CatProducer/test/runtests.sh
+++ CatProducer/test/runtests.sh
@@ -3,17 +3,17 @@
 function die { echo $1: status $2 ;  exit $2; }
 
 cmsRun ${LOCAL_TEST_DIR}/../prod/PAT2CAT_cfg.py \
-  'maxEvents=100' 'runOnMC=1' 'useMiniAOD=1' 'runOnRelVal=1' \
-  'inputFiles=/store/relval/CMSSW_7_6_2/RelValZMM_13/MINIAODSIM/76X_mcRun2_asymptotic_v12-v1/00000/027AB257-A09C-E511-8F3D-003048FFD736.root' \
+  'maxEvents=100' 'runOnMC=1' 'useMiniAOD=1' 'runOnRelVal=0' \
+  'inputFiles=/store/mc/RunIIFall15MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/00000/06532BBC-05C8-E511-A60A-F46D043B3CE5.root' \
   || die 'Failure to run PAT2CAT from MiniAODSIM' $?
 
 cmsRun ${LOCAL_TEST_DIR}/../prod/PAT2CAT_cfg.py \
-  'maxEvents=100' 'runOnMC=1' 'useMiniAOD=1' 'runGenTop=1' 'runOnRelVal=1' \
-  'inputFiles=/store/relval/CMSSW_7_6_2/RelValTTbar_13/MINIAODSIM/76X_mcRun2_asymptotic_v12-v1/00000/0A10678B-8D9C-E511-8AB3-0CC47A4D75F6.root' \
+  'maxEvents=100' 'runOnMC=1' 'useMiniAOD=1' 'runGenTop=1' 'runOnRelVal=0' \
+  'inputFiles=/store/mc/RunIIFall15MiniAODv2/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/00000/00547C97-2FCC-E511-8D75-002590DB91D2.root' \
   || die 'Failure to run PAT2CAT from MiniAODSIM + GenTop' $?
 
 cmsRun ${LOCAL_TEST_DIR}/../prod/PAT2CAT_cfg.py \
   'maxEvents=100' 'runOnMC=0' 'useMiniAOD=1' \
-  'inputFiles=/store/data/Run2015C/DoubleMuon/MINIAOD/PromptReco-v1/000/254/790/00000/22890CFA-034A-E511-A23D-02163E011EC1.root' \
+  'inputFiles=/store/data/Run2015D/DoubleMuon/MINIAOD/PromptReco-v3/000/258/158/00000/F4B20DC1-366C-E511-90F2-02163E011985.root' \
   || die 'Failure to run PAT2CAT from MiniAOD real data' $?
 
