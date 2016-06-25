4478396d8ada415bb0df5781ed7c525efd6557ba
Junghwan John Goh <jhgoh@cern.ch>
2016-06-25 19:51:40 +0200

    json file update

--- CatProducer/python/catDefinitions_cfi.py
+++ CatProducer/python/catDefinitions_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 bunchCrossing  = 25
 globalTag_mc   = '80X_mcRun2_asymptotic_2016_miniAODv2'
 globalTag_rd   = '80X_dataRun2_Prompt_v8'
-lumiJSON       = 'Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON'
+lumiJSON       = 'Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON'
 pileupMCmap    = '2016_25ns_SpringMC'
 
 JetEnergyCorrection = 'Spring16_25nsV3'
