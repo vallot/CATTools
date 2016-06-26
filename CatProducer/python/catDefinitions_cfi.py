859b7e379f00a28957fbfcb9508a34d1e63a4729
Junghwan John Goh <jhgoh@cern.ch>
2016-06-26 11:40:40 +0200

    adjust to correct run range, 275376

--- CatProducer/python/catDefinitions_cfi.py
+++ CatProducer/python/catDefinitions_cfi.py
@@ -4,7 +4,7 @@ bunchCrossing  = 25
 globalTag_mc   = '80X_mcRun2_asymptotic_2016_miniAODv2'
 globalTag_rd   = '80X_dataRun2_Prompt_v8'
 #lumiJSON       = 'Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON'
-lumiJSON       = 'Cert_271036-275125_13TeV_PromptReco_Collisions16_Plus_DCSOnly_275282-275782_JSON'
+lumiJSON       = 'Cert_271036-275125_13TeV_PromptReco_Collisions16_Plus_DCSOnly_275282-275376_JSON'
 pileupMCmap    = '2016_25ns_SpringMC'
 
 JetEnergyCorrection = 'Spring16_25nsV3'
