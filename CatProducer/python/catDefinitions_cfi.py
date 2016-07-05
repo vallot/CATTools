2cb542d482dca8aa0f89db4d7beca3ec164d6480
Junghwan John Goh <jhgoh@cern.ch>
2016-07-05 19:04:59 +0200

    new JEC and JER, JSON and puWeight

--- CatProducer/python/catDefinitions_cfi.py
+++ CatProducer/python/catDefinitions_cfi.py
@@ -1,13 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 bunchCrossing  = 25
-globalTag_mc   = '80X_mcRun2_asymptotic_2016_miniAODv2'
-globalTag_rd   = '80X_dataRun2_Prompt_v8'
-#lumiJSON       = 'Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON'
-lumiJSON       = 'Cert_271036-275125_13TeV_PromptReco_Collisions16_Plus_DCSOnly_275282-275376_JSON'
+globalTag_mc   = '80X_mcRun2_asymptotic_v14'
+globalTag_rd   = '80X_dataRun2_Prompt_v9'
+lumiJSON       = 'Cert_271036-275783_13TeV_PromptReco_Collisions16_JSON_NoL1T'
 pileupMCmap    = '2016_25ns_SpringMC'
 
-JetEnergyCorrection = 'Spring16_25nsV3'
-#JECUncertaintyFile  = 'CATTools/CatProducer/data/JEC/%s_DATA_UncertaintySources_AK4PFchs.txt'%JetEnergyCorrection
-JECUncertaintyFile  = 'CATTools/CatProducer/data/JEC/%s_DATA_Uncertainty_AK4PFchs.txt'%JetEnergyCorrection
+JetEnergyCorrection = 'Spring16_25nsV6'
+JECUncertaintyFile  = 'CATTools/CatProducer/data/JEC/%s_DATA_UncertaintySources_AK4PFchs.txt'%JetEnergyCorrection
+#JECUncertaintyFile  = 'CATTools/CatProducer/data/JEC/%s_DATA_Uncertainty_AK4PFchs.txt'%JetEnergyCorrection
 
