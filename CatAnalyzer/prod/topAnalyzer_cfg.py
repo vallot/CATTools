03b90c664e5c745e971d2c482400ed3e5f62c531
Junghwan John Goh <jhgoh@cern.ch>
2016-12-22 15:43:26 +0900

    use common input file, common JSON file

--- CatAnalyzer/prod/topAnalyzer_cfg.py
+++ CatAnalyzer/prod/topAnalyzer_cfg.py
@@ -11,9 +11,10 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+from CATTools.Validation.commonTestInput_cff import commonTestCATTuples
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:catTuple.root'
+        commonTestCATTuples["sig"]
     )
 )
 
@@ -21,26 +22,27 @@ isData = False
 
 # json file (Only Data)
 if isData:
-  import FWCore.PythonUtilities.LumiList as LumiList
-  process.source.lumisToProcess = LumiList.LumiList(filename = 'Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt').getVLuminosityBlockRange()
+    from CATTools.CatProducer.catDefinitions_cfi import lumiJSON
+    import FWCore.PythonUtilities.LumiList as LumiList
+    process.source.lumisToProcess = LumiList.LumiList(filename = '%s.txt' % lumiJSON).getVLuminosityBlockRange()
 
 process.TopTree = cms.EDAnalyzer('TopAnalyzer',
-                                           genTopLabel   = cms.InputTag("catGenTops"),
-                                           genLabel      = cms.InputTag("prunedGenParticles"),
-                                           muonLabel     = cms.InputTag("catMuons"),
-                                           electronLabel = cms.InputTag("catElectrons"),
-                                           jetLabel      = cms.InputTag("catJets"),
-                                           metLabel      = cms.InputTag("catMETs"),
-                                           pvLabel       = cms.InputTag("catVertex:nGoodPV"),
-                                           puWeight      = cms.InputTag("pileupWeight"),
-                                           genWeightLabel = cms.InputTag("genWeight"),
-                                           triggerBits       = cms.InputTag("TriggerResults::HLT"), 
-                                           triggerObjects = cms.InputTag("catTrigger"),# we don't really use this objects 
-                                           )
+    genTopLabel   = cms.InputTag("catGenTops"),
+    genLabel      = cms.InputTag("prunedGenParticles"),
+    muonLabel     = cms.InputTag("catMuons"),
+    electronLabel = cms.InputTag("catElectrons"),
+    jetLabel      = cms.InputTag("catJets"),
+    metLabel      = cms.InputTag("catMETs"),
+    pvLabel       = cms.InputTag("catVertex:nGoodPV"),
+    puWeight      = cms.InputTag("pileupWeight"),
+    genWeightLabel = cms.InputTag("genWeight"),
+    triggerBits       = cms.InputTag("TriggerResults::HLT"), 
+    triggerObjects = cms.InputTag("catTrigger"),# we don't really use this objects 
+)
 
 process.TFileService = cms.Service("TFileService",
-                                       fileName = cms.string('vallot.root')
-                                   )
+    fileName = cms.string('vallot.root')
+)
 
 
 #process.Tracer = cms.Service("Tracer")
