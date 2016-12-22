495486629d439f8575e1d02778a96fe64495d5d8
Junghwan John Goh <jhgoh@cern.ch>
2016-12-23 01:15:50 +0900

    rollback temporary change

--- Validation/prod/TTLL/pass1.2_cleanup.py
+++ Validation/prod/TTLL/pass1.2_cleanup.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     nSub = 0
     jobsFinished, jobsFailed = [], []
     for sample in os.listdir(outDir):
-        sample = pathjoin(outDir, sample, '.')
+        sample = pathjoin(outDir, sample, 'nominal')
         if not isdir(sample): continue
 
         stat = jobStatus(sample)
