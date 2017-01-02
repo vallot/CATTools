435c3b39e8fe47b7a4a9839f59619cf0782ef25d
Junghwan John Goh <jhgoh@cern.ch>
2017-01-02 21:31:04 +0900

    pt/eta cut to avoid crash

--- CatAnalyzer/python/csvWeights_cfi.py
+++ CatAnalyzer/python/csvWeights_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 csvWeights = cms.EDProducer("CSVWeightProducer",
     src = cms.InputTag("catJets"),
-    minPt = cms.double(15),
-    maxEta = cms.double(2.5),
+    minPt = cms.double(20),
+    maxEta = cms.double(2.41),
 )
