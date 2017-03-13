36bda1407cd7276b04f0f7a3f694a68be5f09ddf
Junghwan John Goh <jhgoh@cern.ch>
2017-03-14 03:20:20 +0900

    use pandas dataframe

--- Validation/prod/common/pass1.3_computeStat.py
+++ Validation/prod/common/pass1.3_computeStat.py
@@ -5,49 +5,38 @@ from ROOT import *
 
 ## Load JSON file and categorize datasets
 import json
+from pandas import DataFrame
 dataDir = "%s/src/CATTools/CatAnalyzer/data/dataset" % os.environ["CMSSW_BASE"]
-js = json.loads(open("%s/dataset.json" % dataDir).read())
+dsets = DataFrame.from_dict(json.loads(open("%s/dataset.json" % dataDir).read()))
 
-dsIn = {}
-for x in js: dsIn[x['name']] = x
+dsets['hist'] = [None]*len(dsets.name)
+dsets['part_nevt'] = [0.0]*len(dsets.name)
+dsets['part_avgWgt'] = [1.0]*len(dsets.name)
+dsets['normFactor'] = [1.0]*len(dsets.name)
 
-ds = {}
-for fName in sorted(os.listdir('pass1')):
-    if not fName.endswith('.root'): continue
-    f = TFile('pass1/'+fName)
-    if f == None:
-        print "!!! root file under %s is invalid" % name
+dsOut = {}
+for index, name in enumerate(dsets.name):
+    fName = 'pass1/%s.root' % name
+    if not os.path.exists(fName):
+        print '!!!', name, 'cannot be found'
         continue
-    d = fName.replace('.root', '')
+    f = TFile(fName)
+    if f == None or f.IsZombie():
+        print '!!!', name, 'cannot opened'
+        continue
+    dsets.loc[index, 'hist'] = fName
 
-    if d in dsIn:
-        ds[d] = dsIn[d]
-    elif '_' in d:
-        origName = '_'.join(d.split('_')[:-1])
-        ds[d] = dict(dsIn[origName])
-        ds[d]['title'] += ":"+d.split('_')[-1]
-    if d not in ds:
-        print "Cannot find corresponding histogram for", d
-
-    ds[d]['hist'] = fName
-
+    dsets.loc[index, 'part_nevt'] = dsets.loc[index, 'nevt']
     h = f.Get("gen/hWeight_Norm")
-    xsec = 1.0
-    normFactor = 1.0
-    avgWgt = float(ds[d]['avgWgt'])
-    nEvent = int(ds[d]['nevt'])
-    partNEvent = 0
     if h != None:
-        xsec = ds[d]['xsec']
-        partAvgWgt = h.GetMean()
-        partNEvent = h.GetEntries()
-        if partNEvent != nEvent or abs(avgWgt-partAvgWgt) > 1e-5:
-            print "!!! Inconsistent stats %s" % d
-            print "    new(%10f, %d) orig(%10f, %d)" % (partAvgWgt, partNEvent, avgWgt, nEvent)
-        normFactor = h.GetEntries()*avgWgt
-    ds[d]['normFactor'] = avgWgt*nEvent
-    ds[d]['part_normFactor'] = normFactor
-    ds[d]['part_nevt'] = partNEvent
+        dsets.loc[index, 'part_nevt'] = h.GetEntries()
+        dsets.loc[index, 'part_avgWgt'] = h.GetMean()
+        dsets.loc[index, 'normFactor'] = h.GetEntries()*h.GetMean()
 
-open("pass1/dataset.json", "w").write(json.dumps(ds, sort_keys=True, indent=4))
+    dsOut[name] = dict(dsets.ix[index]).copy()
 
+subds = dsets[['name', 'nevt', 'part_nevt', 'avgWgt', 'part_avgWgt']]
+print subds[(subds.nevt != subds.part_nevt)]
+
+
+open("pass1/dataset.json", "w").write(json.dumps(dsOut, sort_keys=True, indent=4))
