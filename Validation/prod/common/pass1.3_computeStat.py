229379b405dfcd032c77ebea7bdf8bee6ef39cd5
Junghwan John Goh <jhgoh@cern.ch>
2017-03-14 16:36:38 +0900

    pandas migration

--- Validation/prod/common/pass1.3_computeStat.py
+++ Validation/prod/common/pass1.3_computeStat.py
@@ -9,34 +9,48 @@ from pandas import DataFrame
 dataDir = "%s/src/CATTools/CatAnalyzer/data/dataset" % os.environ["CMSSW_BASE"]
 dsets = DataFrame.from_dict(json.loads(open("%s/dataset.json" % dataDir).read()))
 
-dsets['hist'] = [None]*len(dsets.name)
-dsets['part_nevt'] = [0.0]*len(dsets.name)
-dsets['part_avgWgt'] = [1.0]*len(dsets.name)
-dsets['normFactor'] = [1.0]*len(dsets.name)
+fsets = DataFrame(columns=('hist', 'name', 'nevt', 'avgWgt'))
+for fName in os.listdir('pass1'):
+    if not fName.endswith(".root"): continue
+    f = TFile('pass1/'+fName)
+    if f == None or f.IsZombie(): continue
 
-dsOut = {}
-for index, name in enumerate(dsets.name):
-    fName = 'pass1/%s.root' % name
-    if not os.path.exists(fName):
-        print '!!!', name, 'cannot be found'
-        continue
-    f = TFile(fName)
-    if f == None or f.IsZombie():
-        print '!!!', name, 'cannot opened'
-        continue
-    dsets.loc[index, 'hist'] = fName
+    name = fName.split('.')[0]
 
-    dsets.loc[index, 'part_nevt'] = dsets.loc[index, 'nevt']
     h = f.Get("gen/hWeight_Norm")
+    nevt, avgWgt = 0, 1
     if h != None:
-        dsets.loc[index, 'part_nevt'] = h.GetEntries()
-        dsets.loc[index, 'part_avgWgt'] = h.GetMean()
-        dsets.loc[index, 'normFactor'] = h.GetEntries()*h.GetMean()
+        nevt = h.GetEntries()
+        avgWgt = h.GetMean()
 
-    dsOut[name] = dict(dsets.ix[index]).copy()
+    fsets.loc[fsets.shape[0]] = [fName, name, nevt, avgWgt]
 
-subds = dsets[['name', 'nevt', 'part_nevt', 'avgWgt', 'part_avgWgt']]
-print subds[(subds.nevt != subds.part_nevt)]
+dsets['nevt_new'] = [0.0]*dsets.shape[0]
+dsets['avgWgt_new'] = [1.0]*dsets.shape[0]
+dsOut = {}
+for dsIndex, dsRow in dsets.iterrows():
+    name = dsRow['name']
+    fsRows = fsets[fsets['name'].str.match('^'+name+'$')]
+    if fsRows.shape[0] == 0: continue
 
+    nevt, avgWgt = 0.0, 1.0
+    nevt_orig, avgWgt_orig = dsRow.nevt, dsRow.avgWgt
+    normFactor = 1.0
+    if dsRow.type != 'Data':
+        for fsIndex, fsRow in fsRows.iterrows():
+            nevt += fsRow['nevt']
+            avgWgt += fsRow['avgWgt']*fsRow['nevt']
+        if nevt > 0: avgWgt /= nevt
+        normFactor = nevt*avgWgt
+
+    dsOut[name] = {}
+    dsOut[name].update(dict(dsRow))
+    dsOut[name].update({'nevt':nevt, 'avgWgt':avgWgt, 'normFactor':normFactor})
+
+    dsets.loc[dsIndex, 'nevt_new'] = nevt
+    dsets.loc[dsIndex, 'avgWgt_new'] = avgWgt
+
+subds = dsets[['name', 'nevt', 'nevt_new', 'avgWgt', 'avgWgt_new']]
+print subds[(subds['nevt'] != subds['nevt_new']) | (abs(subds['avgWgt']-subds['avgWgt_new'])>1e-5)]
 
 open("pass1/dataset.json", "w").write(json.dumps(dsOut, sort_keys=True, indent=4))
