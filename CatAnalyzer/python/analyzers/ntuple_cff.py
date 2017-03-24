d3eb739212e411b6c00ea4c421340a9346296d4a
Junghwan John Goh <jhgoh@cern.ch>
2017-03-24 10:11:12 +0900

    better ntupler modification

--- CatAnalyzer/python/analyzers/ntuple_cff.py
+++ CatAnalyzer/python/analyzers/ntuple_cff.py
@@ -1,15 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
-def ntupler_load(process, name):
-    process.load("CATTools.CatAnalyzer.analyzers.ntuple_cfi")
+def ntupler_load(process, selectorName, ntuplerName = "ntuple"):
+    from CATTools.CatAnalyzer.analyzers.ntuple_cfi import ntuple
     for typeName in ['int', 'ints', 'float', 'floats']:
-        for key, val in getattr(process.ntuple, typeName).parameters_().iteritems():
-            setattr(getattr(process.ntuple, typeName), key, cms.InputTag(val.value().replace('eventSel', name)))
-    for key, val in process.ntuple.cands.parameters_().iteritems():
-        getattr(process.ntuple.cands, key).src = cms.InputTag(val.src.value().replace('eventSel', name))
+        for key, val in getattr(ntuple, typeName).parameters_().iteritems():
+            setattr(getattr(ntuple, typeName), key, cms.InputTag(val.value().replace('eventSel', selectorName)))
+    for key, val in ntuple.cands.parameters_().iteritems():
+        getattr(ntuple.cands, key).src = cms.InputTag(val.src.value().replace('eventSel', selectorName))
+
+    setattr(process, ntuplerName, ntuple.clone())
     return process
 
-def ntupler_addVarsTTLL(process, name):
+def ntupler_addVarsTTLL(process, selectorName, ntuplerName = "ntuple"):
     #process.load("CATTools.CatAnalyzer.ttll.ttbarDileptonKinSolutionProducer_cfi")
     from CATTools.CatAnalyzer.ttll.ttbarDileptonKinSolutionProducer_cfi import ttbarDileptonKin
     from CATTools.CatAnalyzer.ttll.ttbarDileptonKinSolutionAlgos_cff import ttbarDileptonKinAlgoPSetDESYSmeared as solverAlgo
@@ -25,21 +27,21 @@ def ntupler_addVarsTTLL(process, name):
     )
 
     process.ttllLeptons = cms.EDProducer("CandViewMerger",
-        src = cms.VInputTag(cms.InputTag(name, "electrons"), cms.InputTag(name, "muons")),
+        src = cms.VInputTag(cms.InputTag(selectorName, "electrons"), cms.InputTag(selectorName, "muons")),
     )
-    process.ttllKin.jets = name+":jets"
+    process.ttllKin.jets = selectorName+":jets"
     process.ttllKin.leptons = "ttllLeptons"
-    process.ttllKin.met = name+":met"
-    process.ttllKin.metphi = name+":metphi"
+    process.ttllKin.met = selectorName+":met"
+    process.ttllKin.metphi = selectorName+":metphi"
 
-    #process.ntuple.floats.weights_genOthers  = cms.InputTag("flatGenWeights", "others")
-    process.ntuple.floats.kin_mLL = cms.InputTag("ttllKin", "mLL")
-    process.ntuple.floats.kin_mLB = cms.InputTag("ttllKin", "mLB")
-    #process.ntuple.floats.kin_mAddJJ = cms.InputTag("ttllKin", "mAddJJ")
-    #process.ntuple.floats.kin_dphi = cms.InputTag("ttllKin", "dphi")
-    process.ntuple.floats.kin_quality = cms.InputTag("ttllKin", "quality")
+    #getattr(process, ntuplerName).floats.weights_genOthers  = cms.InputTag("flatGenWeights", "others")
+    getattr(process, ntuplerName).floats.kin_mLL = cms.InputTag("ttllKin", "mLL")
+    getattr(process, ntuplerName).floats.kin_mLB = cms.InputTag("ttllKin", "mLB")
+    #getattr(process, ntuplerName).floats.kin_mAddJJ = cms.InputTag("ttllKin", "mAddJJ")
+    #getattr(process, ntuplerName).floats.kin_dphi = cms.InputTag("ttllKin", "dphi")
+    getattr(process, ntuplerName).floats.kin_quality = cms.InputTag("ttllKin", "quality")
 
-    process.ntuple.cands.kin = cms.PSet(
+    getattr(process, ntuplerName).cands.kin = cms.PSet(
         src = cms.InputTag("ttllKin"),
         exprsF = cms.untracked.PSet(
             pt  = cms.string("pt"),
@@ -50,10 +52,10 @@ def ntupler_addVarsTTLL(process, name):
     )
     return process
 
-def ntupler_addVarsTTGen(process):
-    process.ntuple.ints.partonTop_modes = cms.InputTag("partonTop", "modes")
+def ntupler_addVarsTTGen(process, ntuplerName = "ntuple"):
+    getattr(process, ntuplerName).ints.partonTop_modes = cms.InputTag("partonTop", "modes")
 
-    process.ntuple.cands.partonTop = cms.PSet(
+    getattr(process, ntuplerName).cands.partonTop = cms.PSet(
         src = cms.InputTag("partonTop"),
         exprsF = cms.untracked.PSet(
             pt  = cms.string("pt"),
@@ -66,7 +68,7 @@ def ntupler_addVarsTTGen(process):
             pdgId = cms.string("pdgId"),
         ),
     )
-    process.ntuple.cands.pseudoTop = cms.PSet(
+    getattr(process, ntuplerName).cands.pseudoTop = cms.PSet(
         src = cms.InputTag("pseudoTop"),
         exprsF = cms.untracked.PSet(
             pt  = cms.string("pt"),
@@ -82,27 +84,27 @@ def ntupler_addVarsTTGen(process):
 
     return process
 
-def ntupler_addVarsGen(process, name):
-    process.ntuple.float.weight = cms.InputTag(name, "weight")
-    process.ntuple.float.weight_gen = cms.InputTag("flatGenWeights", "")
-    process.ntuple.float.weight_pu   = cms.InputTag("pileupWeight")
-    process.ntuple.float.weight_puUp = cms.InputTag("pileupWeight", "up")
-    process.ntuple.float.weight_puDn = cms.InputTag("pileupWeight", "dn")
+def ntupler_addVarsGen(process, selectorName, ntuplerName = "ntuple"):
+    getattr(process, ntuplerName).float.weight = cms.InputTag(selectorName, "weight")
+    getattr(process, ntuplerName).float.weight_gen = cms.InputTag("flatGenWeights", "")
+    getattr(process, ntuplerName).float.weight_pu   = cms.InputTag("pileupWeight")
+    getattr(process, ntuplerName).float.weight_puUp = cms.InputTag("pileupWeight", "up")
+    getattr(process, ntuplerName).float.weight_puDn = cms.InputTag("pileupWeight", "dn")
 
     from CATTools.CatAnalyzer.csvWeights_cfi import csvWeights
-    setattr(process, 'csvWeights'+name, csvWeights.clone(src = cms.InputTag(name, "jets")))
+    setattr(process, 'csvWeights'+selectorName, csvWeights.clone(src = cms.InputTag(selectorName, "jets")))
     del csvWeights
-    process.ntuple.float.weight_CSV = cms.InputTag("csvWeights"+name)
-    process.ntuple.floats.weights_CSV = cms.InputTag("csvWeights"+name, "syst")
+    getattr(process, ntuplerName).float.weight_CSV = cms.InputTag("csvWeights"+selectorName)
+    getattr(process, ntuplerName).floats.weights_CSV = cms.InputTag("csvWeights"+selectorName, "syst")
 
-    process.ntuple.floats.weights_pdf = cms.InputTag("flatGenWeights", "pdf")
-    process.ntuple.floats.weights_scaleUp = cms.InputTag("flatGenWeights", "scaleup")
-    process.ntuple.floats.weights_scaleDn = cms.InputTag("flatGenWeights", "scaledown")
+    getattr(process, ntuplerName).floats.weights_pdf = cms.InputTag("flatGenWeights", "pdf")
+    getattr(process, ntuplerName).floats.weights_scaleUp = cms.InputTag("flatGenWeights", "scaleup")
+    getattr(process, ntuplerName).floats.weights_scaleDn = cms.InputTag("flatGenWeights", "scaledown")
 
     return process
 
-def ntupler_addVarsGenTop(process):
-    process.ntuple.cands.genTop = cms.PSet(
+def ntupler_addVarsGenTop(process, ntuplerName = "ntuple"):
+    getattr(process, ntuplerName).cands.genTop = cms.PSet(
         src = cms.InputTag("catGenTops"),
         index = cms.untracked.int32(0),
         exprsF = cms.untracked.PSet(
