db53eb573a6255e38e5d569d90d798f71e3d2404
SongDongHHHH <zb11kr@naver.com>
2017-05-14 22:28:39 +0900

    20170514 _ no 'nevents' in RD ntuples

--- CatAnalyzer/test/submitBatch.py
+++ CatAnalyzer/test/submitBatch.py
@@ -5,8 +5,8 @@
 import os
 username = os.environ['USER']
 
-analysis = 'h2muAnalyzer'
-#analysis = 'TtbarDiLeptonAnalyzer'
+#analysis = 'h2muAnalyzer'
+analysis = 'TtbarDiLeptonAnalyzer'
 
 pythonCfg = 'run_'+analysis+'_cfg.py'
 #analysis=analysis+'Silver'
@@ -15,6 +15,9 @@ import os,json
 datadir = os.environ["CMSSW_BASE"]+'/src/CATTools/CatAnalyzer/data/dataset/'
 dataset_json = datadir + 'dataset.json'
 version = os.environ["CMSSW_VERSION"]
+mclist = ['TT_powheg', 'WJets', 'SingleTbar_tW', 'SingleTop_tW', 'ZZ', 'WW', 'WZ', 'DYJets', 'DYJets_10to50']
+datalist = ['DoubleEG','DoubleMuon','MuonEG']
+
 
 with open(dataset_json) as data_file:    
     data = json.load(data_file)
@@ -29,12 +32,24 @@ with open(dataset_json) as data_file:
             continue
         if "ttW" in datasetName:
             continue
+        #if not any([d in datasetName for d in mclist]):
+        #    continue
+        #if not "SingleMuon" in datasetName:
+        #    continue
+        #if "DoubleEG_Run2016B" in datasetName:
+        #    continue
+        if not any([d in datasetName for d in datalist]):
+            continue
+        #if not any([d in datasetName for d in mclist]):
+        #    continue
+
+
 
         fileList = datadir + 'dataset_' + datasetName + '.txt'
         jobName = analysis+'_'+datasetName
         #createbatch = "create-batch --cfg %s --jobName %s --fileList %s --maxFiles 10"%(pythonCfg, jobName, fileList) 
         #createbatch = "create-batch --cfg %s --jobName %s --fileList %s --maxFiles 50 --transferDest \"root://cms-xrdr.sdfarm.kr:1094//xrd/store/user/pseudotop/ntuples/%s/%s\""%(pythonCfg, jobName, fileList, version, datasetName)
-        createbatch = "create-batch --cfg %s --jobName %s --fileList %s --maxFiles 50 --transferDest \"root://cms-xrdr.sdfarm.kr:1094//xrd/store/user/%s/cattree/%s/%s\""%(pythonCfg, jobName, fileList, username, version, datasetName)
+        createbatch = "create-batch --cfg %s --jobName %s --fileList %s --maxFiles 40 --transferDest \"root://cms-xrdr.sdfarm.kr:1094//xrd/store/user/king11kr/ntuples_TtbarDstar_v806/CMSSW_8_0_26_patch1/%s\""%(pythonCfg, jobName, fileList,datasetName)
         print createbatch
-        os.system(createbatch)
+        #os.system(createbatch)
         
