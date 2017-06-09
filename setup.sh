e9a0de94545a9dc753683b0968b6819e233c47a5
Geonmo <ry840901@gmail.com>
2017-06-09 16:51:21 +0900

    add glibc-devel

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+yum -y install glibc-devel
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scramv1 project CMSSW CMSSW_8_0_26_patch1
 cd CMSSW_8_0_26_patch1/src
 eval `scramv1 runtime -sh`
-
+gcc -v
 ln -s /CATTools .
 cd CATTools
 git submodule init
@@ -13,4 +14,4 @@ git checkout master
 git pull
 cd ../../..
 
-scram b -j20
+scram b -j2
