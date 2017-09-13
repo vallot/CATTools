40c248c9d9e737f4bab78716fe0ac7724367973f
Jason Lee <jason.lee@cern.ch>
2017-09-14 01:28:25 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,60 @@
-#!/bin/bash
-yum -y install glibc-devel
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-scramv1 project CMSSW CMSSW_8_0_26_patch1
-cd CMSSW_8_0_26_patch1/src
-eval `scramv1 runtime -sh`
-gcc -v
-ln -s /CATTools .
-cd CATTools
-git submodule init
-git submodule update
-cd CommonTools/scripts
-git checkout master
-git pull
+scram project -n v807 CMSSW_8_0_29
+cd v807/src
+cmsenv
+git-cms-init
+git checkout -b cat80x
+
+git-cms-addpkg RecoEgamma/ElectronIdentification
+git-cms-addpkg EgammaAnalysis/ElectronTools
+git-cms-addpkg RecoMET/METFilters
+git-cms-addpkg RecoBTag/DeepFlavour
+
+sed -i 's/badGlobalMuonTagger.clone/badGlobalMuonTaggerMAOD.clone/g' RecoMET/METFilters/python/badGlobalMuonTaggersMiniAOD_cff.py
+
+git cms-merge-topic -u Sam-Harper:HEEPV70VID_8010_ReducedCheckout  #brings in HEEP V70 into VID
+git cms-merge-topic -u ikrav:egm_id_80X_v3 #for other E/gamma IDs in VID if you wish to have them
+git cms-merge-topic -u Sam-Harper:PackedCandNoPuppi #only necessary to run HEEP V70 on AOD
+git-cms-merge-topic -u cms-egamma:EGM_gain_v1
+git-cms-merge-topic -u ikrav:egm_id_80X_v3_photons
+
+cd EgammaAnalysis/ElectronTools/data
+wget https://github.com/cms-egamma/RegressionDatabase/blob/master/SQLiteFiles/GED_80X_Winter2016/ged_regression_20170114.db?raw=true -O ged_regression_20170114.db
+git clone -b Moriond17_gainSwitch_unc https://github.com/ECALELFS/ScalesSmearings.git
 cd ../../..
 
-scram b -j2
+mkdir -p RecoBTag/DeepFlavour/data
+wget http://home.fnal.gov/~verzetti//DeepFlavour/training/DeepFlavourNoSL.json -O  RecoBTag/DeepFlavour/data/DeepFlavourNoSL.json
+wget http://mon.iihe.ac.be/~smoortga/DeepFlavour/CMSSW_implementation_DeepCMVA/Model_DeepCMVA.json -O RecoBTag/DeepFlavour/data/Model_DeepCMVA.json
+
+git clone -b egm_id_80X_v1 https://github.com/ikrav/RecoEgamma-ElectronIdentification.git data1 
+git clone -b egm_id_80X_v1 https://github.com/ikrav/RecoEgamma-PhotonIdentification.git data2
+git clone https://github.com/cms-data/RecoEgamma-ElectronIdentification data3
+
+rsync -avz data1/* RecoEgamma/ElectronIdentification/data/
+rsync -avz data2/* RecoEgamma/ElectronIdentification/data/
+rsync -avz data3/* RecoEgamma/PhotonIdentification/data/
+rm -rf data1 data2 data3
+
+git clone https://github.com/vallot/CATTools
+cd CATTools
+git checkout -b v8-0-7 master ## should be changed
+git submodule init
+git submodule update
+cd ..
+
+## Production only - remove large files because of limitation in crab job file size
+rm -f CATTools/CatAnalyzer/data/KinReco_input.root
+rm -f CATTools/CatAnalyzer/data/KoreaDesyKinRecoInput.root
+rm -f CATTools/CatAnalyzer/data/KoreaKinRecoInput_pseudo.root
+rm -f CATTools/CatAnalyzer/data/desyKinRecoInput.root
+rm -rf RecoEgamma/*Identification/data/Spring15
+rm -rf RecoEgamma/*Identification/data/PHYS14
+
+scram b -j30
+
+catGetDatasetInfo
+
+## Production only - do the unit test
+#cd CATTools/CatProducer
+#voms-proxy-init -voms cms
+#scram b runtests
