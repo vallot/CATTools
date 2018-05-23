66df6de2d2d45fa077d74084aead3d8162925de8
Jiwon <minerva1993@gmail.com>
2018-05-23 11:49:23 +0200

    turn on HLT Ele32 in fcncLepJets

--- CatAnalyzer/plugins/fcncLepJetsAnalyzer.cc
+++ CatAnalyzer/plugins/fcncLepJetsAnalyzer.cc
@@ -1024,11 +1024,15 @@ void fcncLepJetsAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetu
     for (unsigned int i = 0; i < electrons->size() ; i++) {
       const cat::Electron & electron = electrons->at(i);
 
-      if( IsSelectElectron( electron ) ) selectedElectrons.push_back( electron );
-      else if( IsVetoElectron( electron ) ) vetoElectrons.push_back( electron ); // does not Include selected electrons
+      if ( !electron.isHLT_Ele32_WPTight() ) continue;
+
+      if ( IsSelectElectron( electron ) ) selectedElectrons.push_back( electron );
+      else if ( IsVetoElectron( electron ) ) vetoElectrons.push_back( electron ); // does not Include selected electrons
     }
   }
   else if ( !electrons->empty() ) {
+    if ( !(electrons->at(0)).isHLT_Ele32_WPTight() ) continue;
+
     if ( IsSelectElectron(electrons->at(0)) ) selectedElectrons.push_back( electrons->at(0) );
     for (unsigned int i = 1; i < electrons->size() ; i++) {
       const cat::Electron & electron = electrons->at(i);
