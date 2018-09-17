125c2a4db94fecc090c1a620ca73253115be8950
Junghwan John Goh <jhgoh@cern.ch>
2018-09-17 16:29:50 +0900

    scale factor bugfix

--- CommonTools/src/ScaleFactorEvaluator.cc
+++ CommonTools/src/ScaleFactorEvaluator.cc
@@ -24,10 +24,17 @@ void ScaleFactorEvaluator::set(const std::vector<double>& xbins,
 
 double ScaleFactorEvaluator::operator()(const double x, const double y, const double shift) const
 {
+  // Filter out UF and OF
+  if ( x < (*xbins_.begin()) or x >= (*(xbins_.end()-1)) ) return 1;
+  if ( y < (*ybins_.begin()) or y >= (*(ybins_.end()-1)) ) return 1;
+
+  // Special note: std::lower_bound DOES NOT GIVE lower bound of the bin,
+  // it gives the first element which satiesfiles (x < VECTOR_ELEMENT)
+  // - as it is desigend to properly work on integers
   auto xbin = std::lower_bound(xbins_.begin(), xbins_.end(), x);
-  if ( xbin == xbins_.end() or xbin+1 == xbins_.end() ) return 1;
   auto ybin = std::lower_bound(ybins_.begin(), ybins_.end(), y);
-  if ( ybin == ybins_.end() or ybin+1 == ybins_.end() ) return 1;
+  if ( (*xbin) != x ) --xbin;
+  if ( (*ybin) != y ) --ybin;
 
   const int column = xbin-xbins_.begin();
   const int row = ybin-ybins_.begin();
