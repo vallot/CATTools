01ccf566ade6b528ed853216c91856bf52420e30
Jiwon <minerva1993@gmail.com>
2018-12-04 16:23:56 +0100

    use cmssw Muon.h for Selector enum

--- DataFormats/interface/Muon.h
+++ DataFormats/interface/Muon.h
@@ -60,40 +60,11 @@ namespace cat {
 
     float scaleFactor(const std::string& name, int sign = 0) const;
 
-    /// DataFormats/MuonReco/interface/Muon.h
-    /// ====================== STANDARD SELECTORS ===========================
-    ///
-    enum Selector {
-      CutBasedIdLoose        = 1UL<< 0,  
-      CutBasedIdMedium       = 1UL<< 1,  
-      CutBasedIdMediumPrompt = 1UL<< 2,  // medium with IP cuts
-      CutBasedIdTight        = 1UL<< 3,  
-      CutBasedIdGlobalHighPt = 1UL<< 4,  // high pt muon for Z',W' (better momentum resolution)
-      CutBasedIdTrkHighPt    = 1UL<< 5,  // high pt muon for boosted Z (better efficiency)
-      PFIsoVeryLoose         = 1UL<< 6,  // reliso<0.40
-      PFIsoLoose             = 1UL<< 7,  // reliso<0.25
-      PFIsoMedium            = 1UL<< 8,  // reliso<0.20
-      PFIsoTight             = 1UL<< 9,  // reliso<0.15
-      PFIsoVeryTight         = 1UL<<10,  // reliso<0.10
-      TkIsoLoose             = 1UL<<11,  // reliso<0.10
-      TkIsoTight             = 1UL<<12,  // reliso<0.05
-      SoftCutBasedId         = 1UL<<13,  
-      SoftMvaId              = 1UL<<14,  
-      MvaLoose               = 1UL<<15,  
-      MvaMedium              = 1UL<<16,  
-      MvaTight               = 1UL<<17,
-      MiniIsoLoose           = 1UL<<18,  // reliso<0.40
-      MiniIsoMedium          = 1UL<<19,  // reliso<0.20
-      MiniIsoTight           = 1UL<<20,  // reliso<0.10
-      MiniIsoVeryTight       = 1UL<<21   // reliso<0.05
-
-    };
-    
     bool passed( unsigned int selection ) const { return (selectors_ & selection)==selection; }
-    bool passed( Selector selection ) const { return passed(static_cast<unsigned int>(selection)); }
+    bool passed( reco::Muon::Selector selection ) const { return passed(static_cast<unsigned int>(selection)); }
     unsigned int selectors() const { return selectors_; }
     void setSelectors( unsigned int selectors ){ selectors_ = selectors; }
-    void setSelector(Selector selector, bool passed){ 
+    void setSelector(reco::Muon::Selector selector, bool passed){ 
       if (passed) selectors_ |= selector;
       else selectors_ &= ~selector;
     }
