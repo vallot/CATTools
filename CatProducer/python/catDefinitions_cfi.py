b7b5da3757c7920056fd962a07eddac039cf32b2
Jiwon Park <minerva1993@gmail.com>
2019-02-10 13:13:09 +0900

    delete old ttbb configs, usd PPD GT

--- CatProducer/python/catDefinitions_cfi.py
+++ CatProducer/python/catDefinitions_cfi.py
@@ -1,8 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 bunchCrossing  = 25
-globalTag_mc   = '103X_upgrade2018_realistic_v8'
-globalTag_rd   = '103X_dataRun2_Prompt_v3'
+#globalTag_mc   = '103X_upgrade2018_realistic_v8'
+#globalTag_rd   = '103X_dataRun2_Prompt_v3'
+globalTag_mc   = '102X_upgrade2018_realistic_v12'
+globalTag_rd   = '102X_dataRun2_Sep2018Rereco_v1'
+#globalTag_rd   = '102X_dataRun2_Prompt_v11'
 lumiJSON       = 'Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON'
 pileupMCmap    = '2018_25ns_MC'
 
