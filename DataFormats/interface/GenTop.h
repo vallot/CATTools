50521a811252177ebbc9c7680a0cc4b727677fd4
Seohyeon An <klar.wind0425@gmail.com>
2019-05-12 23:48:39 +0900

    can get more than 4 gen b jets

--- DataFormats/interface/GenTop.h
+++ DataFormats/interface/GenTop.h
@@ -107,6 +107,8 @@ namespace cat {
     const math::XYZTLorentzVector addJets1() const { return addJets_[0]; }
     const math::XYZTLorentzVector addJets2() const { return addJets_[1]; }
 
+    const std::vector<math::XYZTLorentzVector> bJets() const { return bJets_; }
+
     //void building( const std::vector<reco::GenJet>* genJets, const std::vector<reco::GenParticle>* genParticles );
     void building( Handle<reco::GenJetCollection> genJets, Handle<reco::GenParticleCollection> genParticles, Handle<std::vector<int> > genBHadFlavour, Handle<std::vector<int> > genBHadJetIndex, Handle<std::vector<int> > genCHadFlavour, Handle<std::vector<int> > genCHadJetIndex  );
 
